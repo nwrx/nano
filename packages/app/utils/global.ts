65d14ca29893c668f3454326d7f44fc3299ee364
Stanley Horwood <stanley@hsjm.io>
2024-08-19 17:37:11 +0200

    chore(app): refactor types and remove unused global.ts
    Removed the unused `global.ts` file from the app's utilities, which contained outdated type definitions for the server application. This file's deletion helps clean up the project's structure and focuses on relevant type definitions.
    
    Updated `types.ts` to include new types for UI components and better organize existing types. Added types for `FlowDragState`, `FlowLinkProps`, `CardProps`, and a union type `DropPayload` to handle different drag-and-drop operations within the application. The restructuring aims to enhance code readability and maintainability.

--- packages/app/utils/global.ts
+++
@@ -1,8 +0,0 @@
-// eslint-disable-next-line @typescript-eslint/consistent-type-imports
-import type { application } from '~/server'
-
-declare module '#imports' {
-  interface Server {
-    application: typeof application
-  }
-}
--- packages/app/utils/types.ts
+++ packages/app/utils/types.ts
@@ -1,19 +1,41 @@
-export interface ChainDragState {
+import type { application } from '~/server'
+import type * as COLORS from './colors'
+
+declare module '@unserved/nuxt/types' {
+  // @ts-expect-error: override the global application type.
+  export type GlobalApplication = typeof application
+}
+
+export type Size = 'large' | 'medium' | 'small'
+export type Variant = keyof typeof COLORS
+export type Alignment = 'center' | 'left' | 'right'
+
+export interface FlowDragState {
   id: string
   color: string
   position: { x: number; y: number }
   kind: 'data' | 'result'
 }
 
-export interface ChainNodePosition {
-  x: number
-  y: number
-}
-
-export interface ChainLinkProps {
+export interface FlowLinkProps {
   sourceX: number
   sourceY: number
   targetX: number
   targetY: number
   color: string
 }
+
+export interface CardProps {
+  dark?: boolean
+  rounded?: boolean
+  centered?: boolean
+  outlined?: boolean
+  gradient?: boolean
+  variant?: Variant
+  as?: keyof HTMLElementTagNameMap
+  classContainer?: string
+}
+
+export type DropPayload =
+  { type: 'createLink'; source: string; target: string } |
+  { type: 'createNode'; kind: string }
