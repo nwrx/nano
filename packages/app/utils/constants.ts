70e4a38a2bc5d101deb9f1ae0df44afbdfc6f570
Stanley Horwood <stanley.horwood@nanoworks.io>
2024-09-26 03:55:32 +0200

    chore(app): add constants file for app configuration
    Introduced a new constants file in the `utils` directory of the `app` package. This file defines key application constants such as the title, description, host, and canonical URL. These constants provide a centralized configuration that can be used throughout the application to maintain consistency in these values.

---
+++ packages/app/utils/constants.ts
@@ -0,0 +1,6 @@
+export const CONSTANTS = {
+  appTitle: 'Nanoworks',
+  appDescription: 'A flow-based programming environment.',
+  appHost: 'app.nwrx.io',
+  appCanonicalUrl: 'https://app.nwrx.io',
+}
