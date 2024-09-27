207803baa7b9653c0716cb9c9ea11417d675e6e9
Stanley Horwood <stanley.horwood@nanoworks.io>
2024-09-27 21:13:21 +0200

    chore(app): refactor and rename middleware for route handling
    Refactored and renamed several middleware functions to improve clarity and reusability. Removed redundant middleware like `guest.ts`, `protected.ts`, and `workspace.ts`. Introduced new middleware such as `abortReserved.ts` to handle navigation aborts for reserved paths, `redirectWhenAuthenticated.ts` to redirect authenticated users, and `redirectWhenGuest.ts` to handle guest user redirections. These changes streamline navigation handling across the application by consolidating similar behaviors and enhancing readability and maintainability of the code.

--- packages/app/middleware/guest.ts
+++
@@ -1,6 +0,0 @@
-import { useClient } from '#imports'
-
-export default defineNuxtRouteMiddleware(async() => {
-  const { isAuthenticated } = await useClient().request('OPTIONS /api/me' )
-  if (isAuthenticated) return navigateTo('/me')
-})
--- packages/app/middleware/workspace.ts
+++
@@ -1,8 +0,0 @@
-const RESERVED_PATH = new Set([
-  '/admin',
-  '/settings',
-])
-
-export default defineNuxtRouteMiddleware(({ path }, _) => {
-  if (RESERVED_PATH.has(path)) return abortNavigation()
-})
