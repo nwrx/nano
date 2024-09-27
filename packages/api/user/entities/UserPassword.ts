a8905f80b4ad7ec65727c32b4defd4d8c08d70d5
Stanley Horwood <stanley.horwood@nanoworks.io>
2024-09-27 23:17:57 +0200

    chore(api): ensure `options` field in `UserPassword` is always provided
    Modified the `UserPassword` entity to ensure the `options` field is always populated. This change enforces that password hashing options are explicitly defined for each instance, enhancing the security and consistency of password management within the system.

--- packages/api/user/entities/UserPassword.ts
+++ packages/api/user/entities/UserPassword.ts
@@ -26,7 +26,7 @@ export class UserPassword extends BaseEntity {
    * The options used to hash the current password of the user.
    */
   @Column('text', { transformer: transformerJson, nullable: true })
-  options?: PasswordOptions
+  options: PasswordOptions
 
   /**
    * The expiration date of the password. It is used to determine when the password
