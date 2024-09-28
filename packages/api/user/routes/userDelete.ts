987796335d80c2ccc4db4dabfaf7d26e73c47103
Stanley Horwood <stanley.horwood@nanoworks.io>
2024-09-28 14:09:34 +0200

    feat(api): enhance user deletion logic with workspace removal
    Updated the `userDelete` function within the API to now handle both user and associated workspace deletion. This modification ensures that when a user is deleted, their corresponding workspace is also removed, maintaining data integrity and cleanliness. This change applies only to users who have the privilege to delete, which is restricted to super administrators and the user themselves. Additionally, the process is encapsulated within a transaction to guarantee that either both entities are successfully deleted or none, enhancing the reliability of the operation.

--- packages/api/user/routes/userDelete.ts
+++ packages/api/user/routes/userDelete.ts
@@ -1,6 +1,8 @@
 import type { ModuleUser } from '../index'
 import { createRoute } from '@unserved/server'
 import { assertStringNotEmpty, createSchema } from '@unshared/validation'
+import { setResponseStatus } from 'h3'
+import { ModuleWorkspace } from '../../workspace'
 
 export function userDelete(this: ModuleUser) {
   return createRoute(
@@ -11,18 +13,28 @@ export function userDelete(this: ModuleUser) {
       }),
     },
     async({ event, parameters }) => {
-      const user = await this.authenticate(event)
+      const workspaceModule = this.getModule(ModuleWorkspace)
+      const { user } = await this.authenticate(event)
       const { username } = parameters
 
-      // --- Check if the request is made by the user or a super administrator.
+      // --- Only super administrators and the user itself can delete the user.
       if (user.username !== username && !user.isSuperAdministrator)
         throw this.errors.USER_NOT_ALLOWED()
 
-      // --- Find the user by the username and soft-remove it.
+      // --- Resolve the user and workspace to remove.
       const { User } = this.getRepositories()
-      const userToDelete = await User.findOne({ where: { username } })
-      if (!userToDelete) return this.errors.USER_NOT_FOUND(username)
-      await User.softRemove(userToDelete)
+      const { Workspace } = workspaceModule.getRepositories()
+      const userToRemove = await this.resolveUser(username)
+      const workspaceToRemove = await Workspace.findOneByOrFail({ name: username })
+
+      // --- Soft-remove the user and workspace.
+      await this.withTransaction(async() => {
+        await User.softDelete({ id: userToRemove.id })
+        await Workspace.softDelete({ id: workspaceToRemove.id })
+      })
+
+      // --- Respond with a 204 status code.
+      setResponseStatus(event, 204)
     },
   )
 }
