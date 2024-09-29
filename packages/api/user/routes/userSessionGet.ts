fc5e55945b0928e4ecb4ffe4a2bd2bfb86094510
Stanley Horwood <stanley.horwood@nanoworks.io>
2024-09-30 00:16:31 +0200

    feat(api): enhance `userSessionGet` route to include user profile details
    Updated the `userSessionGet` function in the `api` package to return more detailed user information, including the user's display name and avatar URL. This change allows clients of the API to retrieve more comprehensive user session data in a single request, improving the user experience by reducing the need to make multiple API calls. Additionally, the function now properly handles cases where a user profile is not found by throwing an error, ensuring better error handling and reliability.

--- packages/api/user/routes/userSessionGet.ts
+++ packages/api/user/routes/userSessionGet.ts
@@ -1,14 +1,28 @@
 import type { ModuleUser } from '../index'
 import { createRoute } from '@unserved/server'
 
+interface UserSessionGetResponse {
+  username?: string
+  avatarUrl?: string
+  displayName?: string
+}
+
 export function userSessionGet(this: ModuleUser) {
   return createRoute(
     {
       name: 'GET /api/session',
     },
-    async({ event }) => {
-      const user = await this.authenticate(event, { optional: true })
-      return { username: user?.user.username }
+    async({ event }): Promise<UserSessionGetResponse> => {
+      const session = await this.authenticate(event, { optional: true })
+      if (!session) return {}
+
+      const user = await this.resolveUser(session.user.username, { profile: true })
+      if (!user.profile) throw new Error('User profile not found')
+      return {
+        username: user.username,
+        displayName: user.profile.displayName,
+        avatarUrl: `/api/users/${user.username}/avatar`,
+      }
     },
   )
 }
