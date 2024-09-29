5ec8a8ba8e1d9213f0faecbc3f4a9a4eab180e94
Stanley Horwood <stanley.horwood@nanoworks.io>
2024-09-29 13:57:12 +0200

    chore(api): removed dependency on module method for session creation
    Refactored the `createUser` function within `createContext.ts` to handle session creation and token generation directly rather than relying on an external module method. This change simplifies the process by directly integrating the session and token creation within the same function, enhancing clarity and control over the session handling mechanics. This update aims to streamline the context setup and make the session management more transparent and maintainable.

--- packages/api/__fixtures__/createContext.ts
+++ packages/api/__fixtures__/createContext.ts
@@ -3,7 +3,7 @@ import type { User } from '../user'
 import type { Workspace, WorkspaceProject } from '../workspace'
 import type { WorkspacePermission, WorkspaceProjectPermission } from '../workspace/utils'
 import { Application } from '@unserved/server'
-import { randomUUID } from 'node:crypto'
+import { createCipheriv, createHash, randomUUID } from 'node:crypto'
 import { rm } from 'node:fs/promises'
 import { request } from 'node:http'
 import { ModuleFlow } from '../flow'
@@ -110,13 +110,26 @@ export async function createContext() {
       // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
       for (const property in options) user[property] = options[property]
 
+      // --- Create the session entity.
+      const { UserSession } = userModule.getRepositories()
+      const session = UserSession.create({
+        user,
+        address: '1.2.3.4',
+        userAgent: 'Vitest',
+        expiresAt: new Date(Date.now() + 1000 * 60 * 60),
+      })
+
+      // --- Create the token for the session.
+      const iv = Buffer.alloc(16, 0)
+      const key = createHash('sha256').update(userModule.userSecretKey).digest()
+      const token = createCipheriv(userModule.userCypherAlgorithm, key, iv).update(session.id).toString('hex')
+
       // --- Create the cookie header.
-      const { session, token } = userModule.createSession(user, { address: '1.2.3.4', userAgent: 'Vitest' })
       const cookie = `${userModule.userSessionCookieName}=${token}`
       const headers = { cookie }
 
       // --- Save the user and workspace.
-      const { User, UserSession } = userModule.getRepositories()
+      const { User } = userModule.getRepositories()
       const { Workspace } = workspaceModule.getRepositories()
       await User.save(user)
       await UserSession.save(session)
