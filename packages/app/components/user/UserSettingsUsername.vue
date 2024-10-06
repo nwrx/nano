8704cb9f0fa4e4ebd1c07c488af70aa9ba29ef2e
Stanley Horwood <stanley.horwood@nanoworks.io>
2024-10-07 01:17:42 +0200

    feat(app): add new user settings components for account, notifications, and username
    Introduced dedicated components for user settings including `UserSettingsAccount`, `UserSettingsNotifications`, and `UserSettingsUsername`. These components provide specific functionality for managing account details, notification preferences, and username updates, enhancing the modularity and maintainability of the user settings management. Each component supports detailed interactions tailored to its purpose, such as updating email and password, managing notification settings across various categories, and modifying the username with associated guidelines. These additions help in segregating responsibilities and improving the user experience by providing a clear interface for each setting type.

---
+++ packages/app/components/user/UserSettingsUsername.vue
@@ -0,0 +1,40 @@
+<script setup lang="ts">
+const props = defineProps<{
+  username: string
+}>()
+
+const emit = defineEmits<{
+  submit: [{ username: string }]
+}>()
+
+const username = useVModel(props, 'username', emit, { passive: true })
+
+function onSubmit() {
+  emit('submit', {
+    username: username.value,
+  })
+}
+</script>
+
+<template>
+  <AppPageForm
+    vertical
+    title="General"
+    label="Save Changes"
+    @submit="() => onSubmit()">
+    <template #text>
+      Update your username. This is your unique identifier and will be used for logging in and other interactions.
+      <br>
+      <br>
+      Note that changing your username might affect how other users recognize you. For more information, please
+      refer to the <Button link variant="primary" href="#">documentation</Button>.
+
+    </template>
+    <InputText
+      v-model="username"
+      icon="i-carbon:user"
+      class="w-full"
+      placeholder="Username"
+    />
+  </AppPageForm>
+</template>
