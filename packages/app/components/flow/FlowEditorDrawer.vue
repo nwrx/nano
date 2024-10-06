4aac0ac3a294e7c810e1adfdf52196cffdc438a3
Stanley Horwood <stanley.horwood@nanoworks.io>
2024-10-07 00:25:53 +0200

    feat(app): extend input zone to keep panel open
    Updated the UI interaction in the `FlowEditorDrawer` component to enhance user experience. The trigger zone for the panel has been extended, allowing the panel to remain open more reliably when users interact with it. This change aims to make the navigation within the flow editor more intuitive and less prone to accidental closures.
    
    The modifications include adjustments to transition properties and restructuring of interactive elements to ensure smoother, more predictable behavior of the drawer panel.

--- packages/app/components/flow/FlowEditorDrawer.vue
+++ packages/app/components/flow/FlowEditorDrawer.vue
@@ -10,8 +10,6 @@ defineProps<{
   <!-- Drawer where all available modules are listed. -->
   <div
     class="
-      flex flex-col items-center justify-start
-      w-16 py-4 space-y-4
       bg-primary-50/20 rounded
       border border-black/10
       backdrop-blur-md
@@ -25,25 +23,27 @@ defineProps<{
       class="group">
 
       <!-- Trigger -->
-      <BaseIcon
-        :icon="category.icon"
-        class="w-16 h-8 transition-all duration-300 cursor-pointer opacity-60 group-hover:opacity-100"
-      />
+      <div class="w-16 my-4 flex items-center justify-center">
+        <BaseIcon
+          load
+          :icon="category.icon"
+          class="w-8 h-8 transition-all text-black duration-300 cursor-pointer opacity-60 group-hover:opacity-100"
+        />
+      </div>
 
       <!-- Menu -->
       <div
         class="
-          absolute bottom-0 left-full pl-4
+          flex items-end
+          absolute bottom-0 top-0 left-full pl-4
           group-hover:opacity-100 opacity-0
-          group-hover:translate-y-0 -translate-y-4
+          group-hover:translate-x-0 -translate-x-4
           group-hover:pointer-events-auto pointer-events-none
           transition-all duration-100
         ">
         <FlowEditorDrawerCategory
           v-bind="category"
-          class="
-            w-64
-          "
+          class="w-64"
         />
       </div>
     </div>
