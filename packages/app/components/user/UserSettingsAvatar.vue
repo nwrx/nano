c3eb35da323e7802fe53857205f69330cb522d3f
Stanley Horwood <stanley.horwood@nanoworks.io>
2024-10-07 00:05:18 +0200

    feat(app): add profile picture update form in user settings
    Introduced a new component `UserSettingsAvatar.vue` within the user settings to allow users to update their profile picture. This form includes a section for uploading a new image and a button for removing the current picture. It supports various image formats and includes a file size limit to ensure optimal performance and user experience. This feature enhances personalization options for users on the platform.

---
+++ packages/app/components/user/UserSettingsAvatar.vue
@@ -0,0 +1,44 @@
+<script setup lang="ts">
+defineProps<{
+  avatarUrl?: string
+}>()
+
+const emit = defineEmits<{
+  submit: [avatarUrl: File]
+}>()
+</script>
+
+<template>
+  <AppPageForm
+    vertical
+    title="Profile Picture">
+    <template #text>
+      Update your profile picture. This can be your photo or any image you like to represent you.
+      Keep in mind that this image will be visible to other users.
+    </template>
+
+    <div class="flex items-center space-x-4 w-full">
+      <InputImage
+        circular
+        label="Upload a new profile picture"
+        accept="image/*"
+        :model-value="avatarUrl"
+        class="max-w-64"
+        @insert="(avatar) => emit('submit', avatar[0])"
+      />
+
+      <div class="w-full py-8">
+        <Button
+          outlined
+          variant="danger"
+          size="sm"
+          icon="i-carbon:trash-can"
+          label="Remove current picture"
+        />
+        <p class="text-sm text-black/60 mt-2">
+          Accepted formats: JPG, PNG, GIF. Max file size: 5MB.
+        </p>
+      </div>
+    </div>
+  </AppPageForm>
+</template>
