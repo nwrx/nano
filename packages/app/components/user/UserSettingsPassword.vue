374fa1abb5c0e9eefdd31bead6482c71fec38137
Stanley Horwood <stanley.horwood@nanoworks.io>
2024-10-10 17:21:35 +0200

    feat(app): add localization to user password settings form
    Implemented localization in the UserSettingsPassword component to enhance user experience across different languages. This includes dynamic placeholders, hints, and button labels that adjust based on the user's locale settings. The update ensures consistency with other localized components within the app and improves accessibility for a diverse user base.

--- packages/app/components/user/UserSettingsPassword.vue
+++ packages/app/components/user/UserSettingsPassword.vue
@@ -3,54 +3,64 @@ const emit = defineEmits<{
   submit: [UserSetPasswordOptions]
 }>()
 
+const { t } = useI18n({ useScope: 'local' })
 const oldPassword = ref('')
 const newPassword = ref('')
 const newPasswordConfirm = ref('')
-
-function onSubmit() {
-  emit('submit', {
-    oldPassword: oldPassword.value,
-    newPassword: newPassword.value,
-    newPasswordConfirm: newPasswordConfirm.value,
-  })
-}
 </script>
 
 <template>
   <AppPageForm
-    vertical
-    title="Password"
-    label="Update Password"
-    @submit="() => onSubmit()">
+    :title="t('title')"
+    :label="t('button.submit')"
+    @submit="() => emit('submit', { oldPassword, newPassword, newPasswordConfirm })">
 
+    <!-- Text -->
     <template #text>
-      Update your password to keep your account secure. It is recommended to use a strong password
-      that you don't use elsewhere and to change it regularly.
+      <I18nT keypath="text">
+        <template #newline><br><br></template>
+      </I18nT>
     </template>
 
+    <!-- Old Password -->
     <InputText
       v-model="oldPassword"
-      icon="i-carbon:password"
-      class="w-full"
-      placeholder="Current Password"
       type="password"
+      class="w-full"
+      icon="i-carbon:password"
+      :placeholder="t('oldPassword.label')"
+      :hint="t('oldPassword.hint')"
     />
 
-    <div class="flex w-full gap-4 lt-md:flex-col">
-      <InputText
-        v-model="newPassword"
-        icon="i-carbon:password"
-        class="w-full"
-        placeholder="New Password"
-        type="password"
-      />
-      <InputText
-        v-model="newPasswordConfirm"
-        icon="i-carbon:password"
-        class="w-full"
-        placeholder="Confirm New Password"
-        type="password"
-      />
-    </div>
+    <!-- New Password -->
+    <InputText
+      v-model="newPassword"
+      type="password"
+      class="w-full"
+      icon="i-carbon:password"
+      :placeholder="t('newPassword.label')"
+    />
+
+    <!-- New Password Confirm -->
+    <InputText
+      v-model="newPasswordConfirm"
+      type="password"
+      class="w-full"
+      icon="i-carbon:password"
+      :placeholder="t('newPasswordConfirm.confirm')"
+      :hint="t('newPassword.hint')"
+    />
   </AppPageForm>
 </template>
+
+<i18n lang="yaml">
+  en:
+    title: Password
+    text: Update your password to keep your account secure. Make sure to use a strong password that is unique to this account.
+    oldPassword.label: Enter your current password
+    oldPassword.hint: Your current password is required to update your password.
+    newPassword.label: Define your new password.
+    newPassword.confirm: Confirm your new password
+    newPassword.hint: Your new password must be at least 8 characters long.
+    button.submit: Save new password
+</i18n>
