b2ec7d6bdcadfc8bea4ecfaed6a46bbb8dc27d34
Stanley Horwood <stanley.horwood@nanoworks.io>
2024-10-12 22:21:10 +0200

    feat(app): enhance localization and UI for user session settings
    Updated the user session settings components to improve localization support and user interface consistency. This includes using the `BaseIcon` component for icons and introducing the new `UserSettingsSessionsItemValue` component for displaying session details. The session details such as IP address, last used time, browser, system, and device are now displayed more uniformly and support localization. This change enhances the readability and accessibility of session information across different languages, aligning with recent localization efforts in other components.

---
+++ packages/app/components/user/UserSettingsSessions.ItemValue.vue
@@ -0,0 +1,10 @@
+<script setup lang="ts">
+defineProps<{ name: string; value: string }>()
+</script>
+
+<template>
+  <div>
+    <p class="text-xs text-subtle">{{ name }}</p>
+    <p class="text-sm text-app">{{ value }}</p>
+  </div>
+</template>
