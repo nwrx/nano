251c98118dd26989a19de47dcd4ab5fa7873d924
Stanley Horwood <stanley.horwood@nanoworks.io>
2024-10-12 17:34:58 +0200

    chore(app): set up Storybook configuration for the app package
    Added initial configurations for Storybook in the `app` package, including `main.ts`, `manager.ts`, `preview.ts`, and `theme.ts`. This setup defines the framework, story sources, addons, and theming consistent with the app's design language. It also includes dynamic reloading capabilities for story and configuration files to streamline development workflows. This enhancement will facilitate better component visualization and interaction testing across the project.

---
+++ packages/app/.storybook/theme.ts
@@ -0,0 +1,45 @@
+import { create } from '@storybook/theming'
+import { COLORS } from '../uno.config'
+
+export const theme = create({
+  base: 'dark',
+
+  // // Typography
+  // fontBase: '"IBM Plex Sans", sans-serif',
+  // fontCode: '"Fira Code", monospace',
+  // gridCellSize: 64,
+
+  // // Brand colors
+  // colorPrimary: COLORS.primary[500],
+  // colorSecondary: COLORS.primary[400],
+
+  // // UI
+  // appBg: COLORS.primary[900],
+  // appContentBg: `${COLORS.primary[50]}20`,
+  // appPreviewBg: COLORS.primary[50],
+  // appBorderColor: `${COLORS.light[500]}40`,
+  // appBorderRadius: 8,
+
+  // booleanBg: COLORS.primary[900],
+  // booleanSelectedBg: COLORS.primary[500],
+
+  // buttonBg: COLORS.primary[400],
+  // buttonBorder: `${COLORS.light[500]}40`,
+
+  // // Text colors
+  // textColor: COLORS.primary[900],
+  // textInverseColor: COLORS.primary[800],
+  // textMutedColor: `${COLORS.primary[800]}80`,
+
+  // // Toolbar default and active colors
+  // barTextColor: `${COLORS.primary[50]}40`,
+  // barSelectedColor: COLORS.primary[50],
+  // barHoverColor: COLORS.primary[50],
+  // barBg: COLORS.primary[900],
+
+  // // Form colors
+  // inputBg: COLORS.primary[900],
+  // inputBorder: `${COLORS.light[500]}40`,
+  // inputTextColor: COLORS.primary[50],
+  // inputBorderRadius: 8,
+})
