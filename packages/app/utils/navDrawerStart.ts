1cc14048438a90109bf59c39338e147f5a9f51c5
Stanley Horwood <stanley.horwood@nanoworks.io>
2024-10-13 00:59:19 +0200

    feat(app): reorganize navigation drawer structure
    The navigation drawer has been reorganized to enhance user navigation and clarity. The changes involve restructuring the primary sections and simplifying the navigation paths by removing redundant routing. The 'Workspace' section has been refined to include only relevant items, and a new 'Settings' section has been added for better access to administrative features. This reorganization aims to improve the overall user experience by making the interface more intuitive and easier to navigate.

--- packages/app/utils/navDrawerStart.ts
+++ packages/app/utils/navDrawerStart.ts
@@ -8,64 +8,57 @@ export const NAV_DRAWER_START = computed<NavItem[]>(() => {
     {
       items: [
         {
-          to: '/',
           label: 'Overview',
           icon: 'i-carbon:home',
         },
         {
-          to: { name: 'Workspace', params: { workspace: session.data.username } },
-          label: 'Workspace',
-          icon: 'i-carbon:flow',
-        },
-        {
-          to: '/modules',
           label: 'Modules',
           icon: 'i-carbon:box',
         },
         {
-          to: '/integrations',
           label: 'Integrations',
           icon: 'i-carbon:integration',
         },
         {
-          to: '/storage',
           label: 'Storage',
           icon: 'i-carbon:db2-database',
-        },
-        {
-          to: '/keys',
-          label: 'API Keys',
-          icon: 'i-carbon:api-key',
         },
       ],
     },
     {
-      label: 'Organization',
+      label: 'Workspace',
       items: [
         {
-          to: '/members',
-          label: 'Members',
-          icon: 'i-carbon:user',
+          label: 'Workspace',
+          icon: 'i-carbon:flow',
+          to: { name: 'Workspace', params: { workspace: session.data.username } },
         },
         {
-          to: '/settings',
-          label: 'UserSettings',
-          icon: 'i-carbon:settings',
-        },
-        {
-          to: '/monitoring',
           label: 'Monitoring',
           icon: 'i-carbon:cloud-monitoring',
         },
         {
-          to: '/usage',
           label: 'Usage',
           icon: 'i-carbon:analytics',
         },
         {
-          to: '/billing',
           label: 'Billing',
           icon: 'i-carbon:document',
+        },
+      ],
+    },
+    {
+      label: 'Settings',
+      items: [
+        {
+          label: 'Users',
+          icon: 'i-carbon:user',
+          to: { name: 'AdminUsers' },
+        },
+        {
+          label: 'Configuration',
+          icon: 'i-carbon:settings',
+          // to: { name: 'AdminConfiguration' },
         },
       ],
     },
