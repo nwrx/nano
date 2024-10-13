ce7e21fcd95b0b4ac0c59e76780c62a1cf3f556a
Stanley Horwood <stanley.horwood@nanoworks.io>
2024-10-13 23:13:07 +0200

    chore(module-core): update color codes for category definitions
    Updated the color codes for the `basic`, `compute`, and `languageModel` categories to better align with the visual design guidelines. These changes improve the visual cohesion across the platform's UI, aiding in user navigation and category recognition.

--- packages/module-core/categories/compute.ts
+++ packages/module-core/categories/compute.ts
@@ -3,7 +3,7 @@ import { defineCategory } from '@nwrx/core'
 export const compute = defineCategory({
   kind: 'compute',
   name: 'Compute',
-  color: '#DB504A',
+  color: '#E75853',
   icon: 'https://api.iconify.design/ph:cpu.svg',
   description: 'A collection of nodes for performing computational operations.',
 })
