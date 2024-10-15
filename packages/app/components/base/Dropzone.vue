85f7defcead7c87c763126561e1881640da20f08
Stanley Horwood <stanley.horwood@nanoworks.io>
2024-10-15 20:05:14 +0200

    feat(app): introduce Dropzone component and improve project item interactions
    Added a new `Dropzone` component to the `base` folder, enhancing the UI for file imports with dynamic visibility and layout changes based on state. This component supports markdown rendering for the drop text, increasing the flexibility and user experience.
    
    Updated the `ProjectList.Item` component to integrate the new Dropzone, enabling file imports directly from the project list. Streamlined event names and updated i18n entries for better localization support. Adjusted the UI to accommodate new functionalities while maintaining a clean layout and interaction cues.

---
+++ packages/app/components/base/Dropzone.vue
@@ -0,0 +1,36 @@
+<script setup lang="ts">
+import { vMarkdown } from '#imports'
+defineProps<{
+  text: string
+  vertical?: boolean
+  isOver?: boolean
+}>()
+</script>
+
+<template>
+  <div
+    :class="{
+      'z-10 op-100': isOver,
+      'z-00 op-0': !isOver,
+      'flex-col space-y-lg': vertical,
+      'flex-row space-x-lg': !vertical,
+    }"
+    class="
+      absolute -top-md -left-md w-[calc(100%+2rem)] h-[calc(100%+0rem)]
+      flex items-center justify-center rd p-md
+      border-2 border-dashed border-app transition
+      pointer-events-none
+      bg-primary-500/5 backdrop-blur-xl
+    ">
+    <BaseIcon
+      icon="i-carbon:cloud-upload"
+      class="text-prominent"
+      :class="vertical ? 'size-12' : 'size-8'"
+    />
+    <p
+      :key="text"
+      v-markdown="text"
+      class="text-app text-2xl"
+    />
+  </div>
+</template>
--- packages/app/components/project/ProjectList.Item.vue
+++ packages/app/components/project/ProjectList.Item.vue
@@ -9,17 +9,20 @@ const props = defineProps<{
 const emit = defineEmits<{
   delete: []
   flowCreate: []
-  flowDelete: [flowSlug: string]
-  flowDownload: [flowSlug: string]
-  flowDuplicate: [flowSlug: string]
+  flowDelete: [flow: string]
+  flowDownload: [flow: string]
+  flowDuplicate: [flow: string]
+  flowImport: [file: File]
   'update:modelValue': [value: boolean]
 }>()
 
-const { t } = useI18n()
+// --- Localize & VModel
+const { t } = useI18n({ useScope: 'local' })
 const model = useVModel(props, 'modelValue', emit, {
   passive: true,
 })
 
+// --- Safely get the project route.
 function getProjectRoute(workspace: string, project: string) {
   if (!workspace || !project) return
   return {
@@ -27,10 +30,27 @@ function getProjectRoute(workspace: string, project: string) {
     params: { workspace, project },
   }
 }
+
+// --- Dropzone for importing flows.
+const dropzone = ref<HTMLDivElement>()
+const { isOverDropZone } = useDropZone(dropzone, {
+  onDrop: (files) => {
+    if (!files) return
+    if (files.length === 0) return
+    emit('flowImport', files[0])
+  },
+})
 </script>
 
 <template>
-  <div class="w-full">
+  <div ref="dropzone" class="w-full h-full relative pb-md">
+
+    <!-- Dropzone -->
+    <Dropzone
+      :isOver="isOverDropZone"
+      :text="t('dropZone', { title })"
+      :vertical="model"
+    />
 
     <!-- Header -->
     <BaseButton
@@ -52,12 +72,8 @@ function getProjectRoute(workspace: string, project: string) {
 
       <!-- Header -->
       <div class="text-left grow">
-        <h3 class="text-lg font-medium">
-          {{ title }}
-        </h3>
-        <p class="text-sm text-subtle line-clamp-2">
-          {{ description }}
-        </p>
+        <h3 class="text-lg font-medium" v-text="title"/>
+        <p class="text-sm text-subtle line-clamp-2" v-text="description"/>
       </div>
 
       <!-- Collaborators -->
@@ -91,12 +107,12 @@ function getProjectRoute(workspace: string, project: string) {
       </ContextMenu>
     </BaseButton>
 
-    <!-- Blocks that lists the flows -->
+    <!-- Flow list -->
     <BaseCollapse
       vertical
       :isOpen="model"
       :class="{ 'opacity-0': !model }"
-      class="border-l border-app ml-lg pl-lg transition-all duration-slow">
+      class="relative border-l border-app ml-lg pl-lg transition-all duration-slow pb-md">
 
       <div class="flex flex-col space-y-xs pt-md mb-md">
         <ProjectListItemFlow
@@ -136,24 +152,29 @@ function getProjectRoute(workspace: string, project: string) {
     menu.access: Access
     menu.delete: Delete
     createFlow: Create a new Flow in this Project
+    dropZone: Import a flow in the **{title}** project.
   fr:
     menu.settings: Modifier
     menu.access: Accès
     menu.delete: Supprimer
     createFlow: Créer un nouveau Flow dans ce projet
+    dropZone: Importer un flux dans le projet **{title}**.
   de:
     menu.settings: Bearbeiten
     menu.access: Zugriff
     menu.delete: Löschen
     createFlow: Erstellen Sie einen neuen Flow in diesem Projekt
+    dropZone: Importieren Sie einen Flow in das Projekt **{title}**.
   es:
     menu.settings: Editar
     menu.access: Acceso
     menu.delete: Borrar
     createFlow: Crear un nuevo flujo en este proyecto
+    dropZone: Importar un flujo en el proyecto **{title}**.
   zh:
     menu.settings: 编辑
     menu.access: 访问
     menu.delete: 删除
     createFlow: 在此项目中创建新流程
+    dropZone: 在 **{title}** 项目中导入流程。
 </i18n>
