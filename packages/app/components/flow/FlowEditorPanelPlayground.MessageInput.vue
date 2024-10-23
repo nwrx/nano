b9a19e64f7d4271e044af95fff6203bf39ab7023
Stanley Horwood <stanley.horwood@nanoworks.io>
2024-10-23 03:08:07 +0200

    feat(app): enhance message handling and user input in FlowEditorPanel
    Introduced a new component `FlowEditorPanelPlaygroundMessageInput` to handle specific event inputs, improving the modularity and readability of the message handling logic within the FlowEditorPanel. This update includes a refactored user input form to support dynamic input fields based on the flow's nodes, enhancing the interactivity and flexibility of the playground.
    
    The changes also involve a clearer separation between input and output message handling, with specific support for markdown rendering in output messages. This enhancement ensures more robust and user-friendly interactions within the flow editor environment.

---
+++ packages/app/components/flow/FlowEditorPanelPlayground.MessageInput.vue
@@ -0,0 +1,18 @@
+<script setup lang="ts">
+import type { FlowSessionEventPayload } from '@nwrx/api'
+
+defineProps<{
+  event: FlowSessionEventPayload<'flow:start'>
+}>()
+</script>
+
+<template>
+  <FlowEditorPanelDataContainer>
+    <FlowEditorPanelData
+      v-for="(input, index) in Object.entries(event.input)"
+      :key="index"
+      :name="input[0]"
+      :modelValue="input[1]"
+    />
+  </FlowEditorPanelDataContainer>
+</template>
