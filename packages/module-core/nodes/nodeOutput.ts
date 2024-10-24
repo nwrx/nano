3b57a267709b1070c645da018739b7dc9309e4cc
Stanley Horwood <stanley.horwood@nanoworks.io>
2024-10-24 20:43:55 +0200

    feat(module-core): refactor input and output node implementations
    Refactored the node implementations for `nodeInput` and `nodeOutput` to enhance maintainability and consistency. Renamed `input.ts` to `nodeInput.ts` and aligned its category naming with other nodes, changing from `basic` to `categoryBasic`. Introduced `nodeOutput.ts` as a new module, standardizing node property names and improving the data schema definitions. Removed the old `output.ts` to consolidate the output functionalities into the new file. This change ensures a more coherent structure and naming convention across the node definitions.

---
+++ packages/module-core/nodes/nodeOutput.ts
@@ -0,0 +1,82 @@
+import type { NodeInstanceContext, Type } from '@nwrx/core'
+import { defineNode } from '@nwrx/core'
+import { defineDataSchema } from '@nwrx/core'
+import { categoryBasic } from '../categories'
+import { boolean, number, object, stream, string } from '../types'
+
+// eslint-disable-next-line @typescript-eslint/consistent-type-definitions
+type OutputDataSchema = {
+  name: string
+  type: 'boolean' | 'number' | 'object' | 'stream' | 'text'
+  value?: any
+}
+
+export const nodeOutput = defineNode({
+  kind: 'output',
+  name: 'Output',
+  icon: 'https://api.iconify.design/carbon:arrow-up.svg',
+  category: categoryBasic,
+  description: ' A value that is sent to an exitpoint in the flow. The value can be any type of data, such as a string, number, or boolean and is provided as an output from the flow.',
+
+  dataSchema: ({ data }: NodeInstanceContext) => {
+    const { type } = data as OutputDataSchema
+    return defineDataSchema<OutputDataSchema>({
+      type: {
+        type: string as Type<'boolean' | 'number' | 'object' | 'stream' | 'text'>,
+        name: 'Type',
+        control: 'select',
+        description: 'The type of the value.',
+        options: [
+          {
+            value: 'text',
+            label: 'Text',
+            icon: 'https://api.iconify.design/carbon:text-long-paragraph.svg',
+            description: 'A text value, such as a string or a paragraph of text.',
+          },
+          {
+            value: 'number',
+            label: 'Number',
+            icon: 'https://api.iconify.design/carbon:character-whole-number.svg',
+            description: 'A numerical value, such as an integer or a decimal number.',
+          },
+          {
+            value: 'boolean',
+            label: 'Yes/No',
+            icon: 'https://api.iconify.design/carbon:checkmark-outline.svg',
+            description: 'A boolean value, such as true or false.',
+          },
+          {
+            value: 'stream',
+            label: 'Stream',
+            icon: 'https://api.iconify.design/carbon:data-1.svg',
+            description: 'A stream of data, such as an audio or video stream.',
+          },
+          {
+            value: 'object',
+            label: 'Object',
+            icon: 'https://api.iconify.design/carbon:json.svg',
+            description: 'An object value, such as a key-value map.',
+          },
+        ],
+      },
+      name: {
+        type: string,
+        name: 'Name',
+        control: 'text',
+        description: 'The property name to send the value to the exitpoint.',
+      },
+      value: {
+        name: 'Value',
+        type: { number, boolean, stream, object, text: string }[type] ?? string,
+        control: 'socket',
+        description: 'The value to send to the exitpoint.',
+        isOptional: true,
+      },
+    })
+  },
+
+  process: ({ data }) => ({
+    name: data.name,
+    value: data.value,
+  }),
+})
