4a89892277699b5ca47b2a035b5bb94c0b8f9c1d
Stanley Horwood <stanley.horwood@nanoworks.io>
2024-11-12 21:11:44 +0100

    chore(app): enhance dialog component structure and styling
    Refactored AppDialog component for better maintainability and performance. Adjusted the model binding and event handling to streamline interaction logic. Enhanced backdrop and dialog transitions for a smoother visual experience. Simplified the i18n keys to align with existing naming conventions, improving code readability and consistency.

--- packages/app/components/app/AppDialog.vue
+++ packages/app/components/app/AppDialog.vue
@@ -1,6 +1,7 @@
+<!-- eslint-disable vue/prop-name-casing -->
 <script setup lang="ts">
 import type { BaseDialogProps } from '@unshared/vue'
-import { vMarkdown } from '#imports'
+import { vMarkdown, type VNode } from '#imports'
 
 const props = defineProps<{
   title?: string
@@ -19,7 +20,14 @@ const emit = defineEmits<{
   confirm: []
 }>()
 
+const slots = defineSlots<{
+  title: () => VNode
+  text: () => VNode
+  actions: () => VNode
+}>()
+
 const { t, locale } = useI18n({ useScope: 'local' })
+const model = useVModel(props, 'modelValue', emit, { passive: true })
 </script>
 
 <template>
@@ -28,112 +36,111 @@ const { t, locale } = useI18n({ useScope: 'local' })
     <!-- Backdrop -->
     <div
       :class="{
-        'op-0': !modelValue,
-        'backdrop-brightness-60 backdrop-blur': modelValue,
+        'op-0': !model,
+        'backdrop-brightness-80 backdrop-blur': model,
       }"
       class="
         fixed left-0 top-0 w-screen h-screen pointer-events-none
-        transition z-100
+        transition duration-fast z-100
       "
+      @mousedown="() => emit('close')"
     />
 
     <!-- Dialog -->
     <LazyBaseDialog
-      v-bind="props"
-      title=""
+      v-slot="slot"
+      v-model="model"
       :class="{
-        'pointer-events-none': !modelValue,
+        'pointer-events-none': !model,
       }"
       class="
         fixed left-0 top-0 w-screen h-screen bg-transparent z-1000
         inline-flex items-center justify-center backdrop:bg-transparent
-        transition dark
+        transition duration-fast dark
       "
       @return="() => emit('confirm')"
       @close="() => emit('close')">
 
       <!-- Content -->
-      <template #dialog="slots">
-        <div
-          class="
+      <div
+        class="
             rd max-w-xl pointer-events-auto
             border border-app bg-app text-app
             transition backdrop-blur-lg
           "
-          :class="{
-            'scale-100 opacity-100 pointer-events-auto': slots.isOpen,
-            'scale-95 opacity-0 pointer-events-none': !slots.isOpen,
-          }">
+        :class="{
+          'scale-100 opacity-100 pointer-events-auto': slot.isOpen,
+          'scale-95 opacity-0 pointer-events-none': !slot.isOpen,
+        }">
 
-          <!-- Title -->
-          <div class="flex items-center justify-between p-md">
-            <slot name="title">
-              <h3 v-markdown="title" />
-            </slot>
-            <BaseButton eager class="group p-sm absolute right-sm" @click="() => slots.close()">
-              <div class="bg-danger-600 opacity-60 group-hover:opacity-100 transition rounded-full size-4" />
-            </BaseButton>
-          </div>
-
-          <!-- Hint -->
-          <div v-if="title || $slots.title" class="flex space-x-md p-md border-x-0 hint rd-0" :class="classHint">
-            <BaseIcon v-if="icon" :icon="icon" class="size-4 shrink-0 mt-xs" />
-            <slot name="text">
-              <p v-markdown="text" class="text-sm" />
-            </slot>
-          </div>
-
-          <!-- Dialog content -->
-          <div class="p-lg w-full" :class="classContent">
-            <slot v-bind="slots" />
-          </div>
-
-          <!-- CTA -->
-          <div class="p-md w-full bg-emphasized">
-            <slot name="actions" v-bind="slots">
-              <div class="flex items-center justify-between w-full">
-                <Button
-                  :label="labelCancel ?? t('button.cancel')"
-                  icon="i-carbon:close"
-                  size="sm"
-                  class="mr-xl"
-                  link
-                  @click="() => slots.close()"
-                />
-                <Button
-                  light
-                  outlined
-                  :variant="variant ?? 'success'"
-                  :label="labelConfirm ?? t('button.confirm')"
-                  icon-append="i-carbon:chevron-right"
-                  icon-expand
-                  size="sm"
-                  :disabled="disabled"
-                  @click="() => slots.returnValue(true)"
-                />
-              </div>
-            </slot>
-          </div>
+        <!-- Title -->
+        <div class="flex items-center justify-between p-md">
+          <slot name="title">
+            <h3 v-markdown="title" />
+          </slot>
+          <BaseButton eager class="group p-sm absolute right-sm" @click="() => slot.close()">
+            <div class="bg-danger-600 opacity-60 group-hover:opacity-100 transition rounded-full size-4" />
+          </BaseButton>
         </div>
-      </template>
+
+        <!-- Hint -->
+        <div v-if="title || slots.title()" class="flex space-x-md p-md border-x-0 hint rd-0" :class="classHint">
+          <BaseIcon v-if="icon" :icon="icon" class="size-4 shrink-0 mt-xs" />
+          <slot name="text">
+            <p v-markdown="text" class="text-sm" />
+          </slot>
+        </div>
+
+        <!-- Dialog content -->
+        <div class="p-lg w-full" :class="classContent">
+          <slot v-bind="slot" />
+        </div>
+
+        <!-- CTA -->
+        <div class="p-md w-full bg-emphasized">
+          <slot name="actions" v-bind="slot">
+            <div class="flex items-center justify-between w-full">
+              <Button
+                :label="labelCancel ?? t('cancel')"
+                icon="i-carbon:close"
+                size="sm"
+                class="mr-xl"
+                link
+                @click="() => slot.close()"
+              />
+              <Button
+                light
+                outlined
+                :variant="variant ?? 'success'"
+                :label="labelConfirm ?? t('confirm')"
+                icon-append="i-carbon:chevron-right"
+                icon-expand
+                size="sm"
+                :disabled="disabled"
+                @click="() => slot.returnValue(true)"
+              />
+            </div>
+          </slot>
+        </div>
+      </div>
     </LazyBaseDialog>
   </Teleport>
 </template>
 
 <i18n lang="yaml">
 en:
-  button.cancel: Cancel
-  button.confirm: Confirm
+  cancel: Cancel
+  confirm: Confirm
 fr:
-  button.cancel: Annuler
-  button.confirm: Confirmer
+  cancel: Annuler
+  confirm: Confirmer
 de:
-  button.cancel: Abbrechen
-  button.confirm: Bestätigen
+  cancel: Abbrechen
+  confirm: Bestätigen
 es:
-  button.cancel: Cancelar
-  button.confirm: Confirmar
+  cancel: Cancelar
+  confirm: Confirmar
 zh:
-  button.cancel: 取消
-  button.confirm: 确认
+  cancel: 取消
+  confirm: 确认
   </i18n>
