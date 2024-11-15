28dae798ea633326852611a1463cfed1c0700ccd
Stanley Horwood <stanley.horwood@nanoworks.io>
2024-11-15 22:23:52 +0100

    feat(module-core): enhance parsing capabilities for `object` type
    Updated the parsing logic in the `object` type definition to better handle different input scenarios. Previously, the parser only validated objects, but now it includes additional rules to parse and validate JSON strings and return raw input if necessary. This enhancement ensures more robust data handling and provides clearer error messages for incorrect inputs.

--- packages/module-core/types/object.ts
+++ packages/module-core/types/object.ts
@@ -1,10 +1,14 @@
 import { defineType } from '@nwrx/core'
-import { assertObject, createParser } from '@unshared/validation'
+import { assertObject, assertString, createRuleSet } from '@unshared/validation'
 
 export const object = defineType({
   kind: 'object',
   name: 'Object',
   color: '#BD3249',
   description: 'A collection of key-value pairs.',
-  parse: createParser(assertObject<Record<string, unknown>>),
+  parse: createRuleSet(
+    [assertObject<Record<string, unknown>>],
+    [assertString, value => JSON.parse(value as string) as Record<string, unknown>],
+    [(value: unknown) => ({ value })],
+  ),
 })
