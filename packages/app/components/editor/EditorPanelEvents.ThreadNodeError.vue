38d5c68b1cca20897b6096238f493b83b9f595e5
Stanley Horwood <stanley.horwood@nanoworks.io>
2024-11-15 01:20:02 +0100

    feat(app): enhance error display in EditorPanelEvents.ThreadNodeError
    Improved the error display in `EditorPanelEvents.ThreadNodeError` by adding a badge to show error codes and wrapping the error message in a paragraph with a smaller font size. This change provides better visual separation and emphasis on different parts of the error details, improving readability and user experience in debugging scenarios.

--- packages/app/components/editor/EditorPanelEvents.ThreadNodeError.vue
+++ packages/app/components/editor/EditorPanelEvents.ThreadNodeError.vue
@@ -7,7 +7,19 @@ defineProps<{
 </script>
 
 <template>
-  <div class="hint hint-danger">
-    {{ event.message }}
+  <div class="hint hint-danger select-text">
+    <!-- Code -->
+    <Badge
+      v-if="event.code"
+      :label="event.code"
+      class="badge-sm badge-danger mb-xs font-mono"
+    />
+
+    <!-- Error -->
+    <p class="text-sm">
+      <slot>
+        {{ event.message }}
+      </slot>
+    </p>
   </div>
 </template>
