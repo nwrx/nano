4ae8349d3df509b5afca06cc70076e19fa71f63c
Pierre SARAZIN <pierre.sarazin@nanoworks.io>
2024-11-30 23:55:55 +0100

    feat(module-core): add MistralAI node for language model integration

---
+++ packages/module-core/nodes/nodeModelMistralai.ts
@@ -0,0 +1,23 @@
+import type { OpenaiChatRequest } from '../utils'
+import type { InferenceData } from './nodeInference'
+import { defineLanguageModel, openaiGetBody, openaiGetModels } from '../utils'
+
+export const nodeModelMistralai = defineLanguageModel({
+  name: 'MistralAI',
+  kind: 'core/mistralai',
+  icon: 'https://api.iconify.design/logos:mistral-ai-icon.svg',
+  description: 'The **MistralAI API** node is designed to retreive a **Language Model Instance** that can be used to generate completions using the MistralAI API. The node requires an API key and a model name as input, and returns the model information required for generating completions.',
+
+  defaultUrl: 'https://api.mistral.ai',
+  defaultModel: 'open-mistral-7b',
+  pathModels: '/v1/models',
+  pathCompletions: '/v1/chat/completions',
+  allowCustomToken: true,
+  allowCustomModel: true,
+
+  getBody: (data: InferenceData) => {
+    const { max_completion_tokens, seed, ...body } = openaiGetBody(data)
+    return { max_tokens: max_completion_tokens, ...body } as OpenaiChatRequest
+  },
+  getModels: openaiGetModels,
+})
