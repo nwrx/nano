708884f5cb5af09f0a68b566699d6cc06a15a7de
Stanley Horwood <stanley.horwood@nanoworks.io>
2024-12-12 15:29:25 +0100

    chore(app): update AppPage.Header component and add AppPage.HeaderTab
    Updated the AppPage.Header component to support both single and multiple titles using the MaybeArray type. This enhancement improves the flexibility of the header component by allowing it to accommodate various title formats seamlessly. Additionally, the layout of the component has been modified to be sticky at the top with a new styling scheme that enhances its visibility and functionality within the application interface.
    
    Introduced a new component, AppPage.HeaderTab, which utilizes the BaseButtonProps for better interactivity and consistency across different parts of the application. This component is designed to be used within the AppPage.Header for better navigation and user experience, providing a clear and interactive way to switch between different views or sections.

--- packages/app/components/app/AppPage.Header.vue
+++ packages/app/components/app/AppPage.Header.vue
@@ -1,7 +1,9 @@
 <script setup lang="ts">
+import type { MaybeArray } from '@unshared/types'
+
 defineProps<{
   icon?: string
-  title: string
+  title: MaybeArray<string>
   description: string
   badge?: string
   badgeVariant?: Variant
@@ -9,28 +11,38 @@ defineProps<{
 </script>
 
 <template>
-  <div class="w-full p-lg pb-0">
-    <div
-      class="
-        flex flex-col justify-start w-full p-lg h-48
-        bg-layout text-layout rounded
-      ">
+  <div
+    class="
+      sticky top-0 w-full z-1 b-b b-app
+      flex justify-between w-full px-lg h-24
+      text-app shrink-0 bg-app
+    ">
 
-      <!-- Title -->
-      <div class="flex items-center gap-md self-start">
-        <div class="bg-prominent rounded p-sm">
-          <BaseIcon v-if="icon" :icon="icon" class="text-prominent size-5" />
-        </div>
-        <span class="text-4xl font-medium ">
-          {{ title }}
-        </span>
+    <!-- Title -->
+    <div class="flex items-center gap-md self-center">
+
+      <!-- Icon -->
+      <div v-if="icon" class="bg-prominent rounded p-sm mr-md">
+        <BaseIcon :icon="icon" class="text-prominent size-8" />
       </div>
 
-      <!-- Breadcrumb -->
-      <RouterLink to="/" class="flex items-center gap-sm self-end mt-auto">
-        <BaseIcon icon="i-carbon:chevron-right" class="size-4" />
-        <span>{{ title }}</span>
-      </RouterLink>
+      <!-- Multiple Titles -->
+      <template v-if="Array.isArray(title)">
+        <div v-for="(x, index) in title" :key="x" class="flex items-center space-x-sm">
+          <BaseIcon v-if="index > 0" icon="i-carbon:chevron-right" class="size-6 mr-sm" />
+          <span class="text-4xl font-medium">{{ x }}</span>
+        </div>
+      </template>
+
+      <!-- Single Title -->
+      <span v-else class="text-4xl font-medium ">
+        {{ title }}
+      </span>
+    </div>
+
+    <!-- Tabs -->
+    <div class="flex items-center justify-end gap-md mt-auto">
+      <slot />
     </div>
   </div>
 </template>
