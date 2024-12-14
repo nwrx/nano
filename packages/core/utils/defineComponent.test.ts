ade758ff81e028ad7900b06cd5651cd2352dea1b
Stanley Horwood <stanley.horwood@nanoworks.io>
2024-12-14 20:38:39 +0100

    feat(core): refactor `defineComponent` and update associated tests
    Refined the `defineComponent` function and its tests to enhance clarity and maintainability. Removed outdated properties like `name` and `version` from the component definition, aligning it with the new design philosophy that emphasizes simplicity and reusability. The test file now reflects these changes and includes updated assertions that focus on the essential properties of components without redundant attributes. This update helps streamline component creation and improves the overall development experience within the core package.

--- packages/core/utils/defineComponent.test.ts
+++ packages/core/utils/defineComponent.test.ts
@@ -1,22 +1,18 @@
-/* eslint-disable vue/component-definition-name-casing */
 import { defineComponent, SYMBOL_COMPONENT } from './defineComponent'
 
 describe('defineComponent', () => {
   it('should define a component with the given options', () => {
     const component = defineComponent({
-      name: 'example',
-      version: '1.0.0',
       icon: 'https://example.com/icon.svg',
       title: 'Example Component',
       description: 'This is an example component.',
       inputs: { input1: { type: 'string' } },
       outputs: { output1: { type: 'string' } },
+      process: vi.fn(),
     })
 
     expect(component).toStrictEqual({
-      [SYMBOL_COMPONENT]: true,
-      name: 'example',
-      version: '1.0.0',
+      ['@instanceOf']: SYMBOL_COMPONENT,
       icon: 'https://example.com/icon.svg',
       title: 'Example Component',
       description: 'This is an example component.',
@@ -26,14 +22,17 @@ describe('defineComponent', () => {
     })
   })
 
-  it('should use the name as the title if the title is not provided', () => {
-    const component = defineComponent({ name: 'example', version: '1.0.0' })
-    expect(component.title).toBe('example')
-  })
-
   it('should define a component with a process function', () => {
     const process = vi.fn()
-    const component = defineComponent({ name: 'example', version: '1.0.0' }, process)
-    expect(component.process).toBe(process)
+    const component = defineComponent({}, process)
+    expect(component).toStrictEqual({
+      ['@instanceOf']: SYMBOL_COMPONENT,
+      process,
+      description: undefined,
+      icon: undefined,
+      inputs: undefined,
+      outputs: undefined,
+      title: undefined,
+    })
   })
 })
