5a1a27f08263d93bf2db60f0ce61f5fb0873c61e
Stanley Horwood <stanley.horwood@nanoworks.io>
2024-12-18 21:17:55 +0100

    feat(core): enhance handling of undefined or null values in schema resolution
    Introduced a new utility function `resolveSchemaUndefinedOrNull` to better handle scenarios where schema values are undefined or null. This function streamlines the handling of default values and optional fields within the schema resolution process. It includes capabilities to return default values, deeply clone default objects, and ensure these objects are immutable by freezing them.
    
    This change improves the robustness and security of the schema resolution process, particularly in environments where schema defaults might involve executable code or complex data structures. The addition of comprehensive tests ensures that the new functionality behaves as expected across various edge cases, including handling required fields and applying default functions.

--- packages/core/utils/resolveSchema.test.ts
+++ packages/core/utils/resolveSchema.test.ts
@@ -2,39 +2,165 @@ import { ERRORS as E } from './errors'
 import { resolveSchema } from './resolveSchema'
 
 describe('resolveSchema', () => {
-  it('should resolve a valid string', async() => {
-    const result = await resolveSchema('value', 'Hello, World!', { type: 'string' })
-    expect(result).toBe('Hello, World!')
+  describe('references', () => {
+    it('should resolve a reference when resolvers are provided', async() => {
+      const resolver = () => 'Hello, World!'
+      const result = await resolveSchema('value', { $ref: '#/Variables/Greet' }, { type: 'string' }, [resolver])
+      expect(result).toBe('Hello, World!')
+    })
+
+    it('should throw an error if the reference is not resolved', async() => {
+      const shouldThrow = resolveSchema('value', { $ref: '#/Variables/Greet' }, { type: 'string' })
+      const error = E.REFERENCE_NOT_RESOLVED('value')
+      await expect(shouldThrow).rejects.toThrow(error)
+    })
+
+    it('should throw an error if the reference value is undefined', async() => {
+      const resolver = () => undefined
+      const shouldThrow = resolveSchema('value', { $ref: '#/Variables/Greet' }, { type: 'string' }, [resolver])
+      const error = E.REFERENCE_NOT_RESOLVED('value')
+      await expect(shouldThrow).rejects.toThrow(error)
+    })
+
+    it('should throw an error if the reference value does not match the schema', async() => {
+      const resolver = () => 42
+      const shouldThrow = resolveSchema('value', { $ref: '#/Variables/Greet' }, { type: 'string' }, [resolver])
+      const error = E.INPUT_NOT_STRING('value')
+      await expect(shouldThrow).rejects.toThrow(error)
+    })
+
+    it('should resolve object with nested references', async() => {
+      const resolver = () => 'Hello, World!'
+      const result = await resolveSchema(
+        'value',
+        { foo: { $ref: '#/Variables/Greet' } },
+        { type: 'object', properties: { foo: { type: 'string' } } },
+        [resolver],
+      )
+      expect(result).toEqual({ foo: 'Hello, World!' })
+    })
   })
 
-  it('should resolve a valid number', async() => {
-    const result = await resolveSchema('value', 42, { type: 'number' })
-    expect(result).toBe(42)
+  describe('untyped schema', () => {
+    it('should accept string values', async() => {
+      const result = await resolveSchema('value', 'Hello, World!', {})
+      expect(result).toBe('Hello, World!')
+    })
+
+    it('should accept number values', async() => {
+      const result = await resolveSchema('value', 42, {})
+      expect(result).toBe(42)
+    })
+
+    it('should accept boolean values', async() => {
+      const result = await resolveSchema('value', true, {})
+      expect(result).toBe(true)
+    })
+
+    it('should accept array values', async() => {
+      const result = await resolveSchema('value', [1, 2, 3], {})
+      expect(result).toEqual([1, 2, 3])
+    })
+
+    it('should accept object values', async() => {
+      const result = await resolveSchema('value', { foo: 'bar' }, {})
+      expect(result).toEqual({ foo: 'bar' })
+    })
   })
 
-  it('should resolve a valid boolean', async() => {
-    const result = await resolveSchema('value', true, { type: 'boolean' })
-    expect(result).toBe(true)
+  describe('schema validation', () => {
+
+    it('should resolve a valid string', async() => {
+      const result = await resolveSchema('value', 'Hello, World!', { type: 'string' })
+      expect(result).toBe('Hello, World!')
+    })
+
+    it('should resolve a valid number', async() => {
+      const result = await resolveSchema('value', 42, { type: 'number' })
+      expect(result).toBe(42)
+    })
+
+    it('should resolve a valid boolean', async() => {
+      const result = await resolveSchema('value', true, { type: 'boolean' })
+      expect(result).toBe(true)
+    })
+
+    it('should resolve a valid array', async() => {
+      const result = await resolveSchema('value', [1, 2, 3], { type: 'array', items: { type: 'number' } })
+      expect(result).toEqual([1, 2, 3])
+    })
+
+    it('should resolve a valid object', async() => {
+      const result = await resolveSchema('value', { foo: 'bar' }, { type: 'object', properties: { foo: { type: 'string' } } })
+      expect(result).toEqual({ foo: 'bar' })
+    })
   })
 
-  it('should resolve a valid array', async() => {
-    const result = await resolveSchema('value', [1, 2, 3], { type: 'array', items: { type: 'number' } })
-    expect(result).toEqual([1, 2, 3])
+  describe('x-optional', () => {
+    it('should return undefined if the value is undefined and optional', async() => {
+      const result = await resolveSchema('value', undefined, { 'type': 'string', 'x-optional': true })
+      expect(result).toBeUndefined()
+    })
+
+    it('should return undefined if the value is null and optional', async() => {
+      const result = await resolveSchema('value', null, { 'type': 'string', 'x-optional': true })
+      expect(result).toBeUndefined()
+    })
+
+    it('should return the default value if the value is undefined and optional', async() => {
+      const result = await resolveSchema('value', undefined, { 'type': 'string', 'x-optional': true, 'default': 'Hello, World!' })
+      expect(result).toBe('Hello, World!')
+    })
+
+    it('should return the default value if the value is null and optional', async() => {
+      const result = await resolveSchema('value', null, { 'type': 'string', 'x-optional': true, 'default': 'Hello, World!' })
+      expect(result).toBe('Hello, World!')
+    })
   })
 
-  it('should resolve a valid object', async() => {
-    const result = await resolveSchema('value', { foo: 'bar' }, { type: 'object', properties: { foo: { type: 'string' } } })
-    expect(result).toEqual({ foo: 'bar' })
+  describe('x-default', () => {
+    it('should return the default value if the value is undefined', async() => {
+      const result = await resolveSchema('value', undefined, { 'type': 'string', 'x-default': () => 'Hello, World!' })
+      expect(result).toStrictEqual('Hello, World!')
+    })
+
+    it('should return the default value if the value is null', async() => {
+      const result = await resolveSchema('value', null, { 'type': 'object', 'x-default': () => ({ foo: 'bar' }) })
+      expect(result).toStrictEqual({ foo: 'bar' })
+    })
   })
 
-  it('should throw an error if the value is undefined and not optional', async() => {
-    const shouldThrow = resolveSchema('value', undefined, { type: 'string' })
-    const error = E.INPUT_REQUIRED('value')
-    await expect(shouldThrow).rejects.toThrow(error)
+  describe('default', () => {
+    it('should return the default value if the value is undefined', async() => {
+      const result = await resolveSchema('value', undefined, { type: 'string', default: 'Hello, World!' })
+      expect(result).toStrictEqual('Hello, World!')
+    })
+
+    it('should return the default value if the value is null', async() => {
+      const result = await resolveSchema('value', null, { type: 'string', default: 'Hello, World!' })
+      expect(result).toStrictEqual('Hello, World!')
+    })
+
+    it('should return a deep clone of the default value', async() => {
+      const defaultValue = { foo: 'bar' }
+      const result = await resolveSchema('value', undefined, { type: 'object', default: defaultValue })
+      expect(result).toStrictEqual(defaultValue)
+      expect(result).not.toBe(defaultValue)
+    })
   })
 
-  it('should return default value if the value is undefined and optional', async() => {
-    const result = await resolveSchema('value', undefined, { 'type': 'string', 'x-optional': true, 'default': 'default' })
-    expect(result).toBe('default')
+  describe('required', () => {
+    it('should throw an error if the value is required', async() => {
+      const shouldThrow = resolveSchema('value', undefined, { type: 'string' })
+      const error = E.INPUT_REQUIRED('value')
+      await expect(shouldThrow).rejects.toThrow(error)
+    })
+
+    it('should throw if reference is null and value is required', async() => {
+      const resolver = () => null
+      const shouldThrow = resolveSchema('value', { $ref: '#/Variables/Greet' }, { type: 'string' }, [resolver])
+      const error = E.INPUT_REQUIRED('value')
+      await expect(shouldThrow).rejects.toThrow(error)
+    })
   })
 })
