14a6ef3a68f7340b9f7ce74c56fb39b8ee62d7d5
Stanley Horwood <stanley.horwood@nanoworks.io>
2024-12-18 01:32:59 +0100

    feat(core): add support for abort signal in sandbox processing
    Introduced handling for the abort signal in the `processInSandbox` function to enhance control over asynchronous operations within isolated environments. This update allows for the immediate release and disposal of resources when an abort signal is triggered, ensuring better resource management and error handling in sandbox operations. This feature is crucial for preventing potential memory leaks and ensuring the stability of applications that rely on sandboxed code execution.

--- packages/core/sandbox/processInSandbox.ts
+++ packages/core/sandbox/processInSandbox.ts
@@ -1,11 +1,16 @@
 import type { Function, ObjectLike } from '@unshared/types'
 import type { ProcessContext } from '../utils/defineComponent'
 import ivm from 'isolated-vm'
+import { ERRORS as E } from '../utils'
 import { implementFetch } from './implementFetch'
 import { implementHeaders } from './implementHeaders'
 import { implementTextDecoder } from './implementTextDecoder'
 import { implementTextEncoder } from './implementTextEncoder'
 import { wrapInSandbox } from './wrapInSandbox'
+
+export interface ProcessInSandboxOptions extends ProcessContext {
+  abortSignal?: AbortSignal
+}
 
 /**
  * Process the given function in a sandbox.
@@ -14,18 +19,30 @@ import { wrapInSandbox } from './wrapInSandbox'
  * @param context The data and trace to pass to the function.
  * @returns The result of the function call.
  */
-export async function processInSandbox(fn: Function, context: ProcessContext): Promise<ObjectLike> {
+export async function processInSandbox(fn: Function | string, context: ProcessInSandboxOptions): Promise<ObjectLike> {
   const fnWrapped = await wrapInSandbox(fn, { timeout: 10000 })
 
-  // --- Restore "some" WebAPI functions.
+  // --- Implement some WebAPI functions.
   await implementFetch(fnWrapped.isolate, fnWrapped.context)
   await implementHeaders(fnWrapped.isolate, fnWrapped.context)
   await implementTextDecoder(fnWrapped.isolate, fnWrapped.context)
   await implementTextEncoder(fnWrapped.isolate, fnWrapped.context)
 
-  // --- Call the function in the sandbox.
-  return await fnWrapped({
-    data: context.data,
-    trace: new ivm.Callback((data: ObjectLike) => context.trace(data)),
-  }) as ObjectLike
+  // --- Watch for the abort signal and dispose the context if it's aborted.
+  return new Promise((resolve, reject) => {
+    context.abortSignal?.addEventListener('abort', () => {
+      fnWrapped.context.release()
+      fnWrapped.isolate.dispose()
+      const error = E.ISOLATED_VM_DISPOSED()
+      reject(error)
+    })
+
+    // --- Call the function in the sandbox.
+    void fnWrapped({
+      data: context.data,
+      trace: new ivm.Callback((data: ObjectLike) => context.trace(data)),
+    })
+      .then(resolve)
+      .catch(reject)
+  })
 }
