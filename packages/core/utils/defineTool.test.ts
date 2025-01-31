87b67b48d56ec4ef3d524309155d3797e6296169
Stanley Horwood <stanley.horwood@nanoworks.io>
2025-01-31 19:08:38 +0100

    chore(core): define and test the `defineTool` functionality
    Introduced the `defineTool` function and related tests to streamline the creation of tools within nodes. This update includes new utility files and modifications to existing ones to accommodate the tool definition and its schema. The `defineTool` function simplifies the process of defining tools by encapsulating properties and behaviors into a structured format, ensuring consistency across tool implementations. Additionally, comprehensive tests have been added to validate the functionality and ensure that tools are created with the correct properties and behaviors without including any extra, unintended properties.

---
+++ packages/core/utils/defineTool.test.ts
@@ -0,0 +1,72 @@
+import { defineTool } from './defineTool'
+
+describe('createTool', () => {
+  it('should create a tool with the given properties', () => {
+    const call = () => Promise.resolve({ weather: 'sunny' })
+    const result = defineTool('example', {
+      call,
+      name: 'Weather Forecast',
+      description: 'A tool that can be used to get the weather forecast.',
+      properties: {
+        location: {
+          type: 'string',
+          description: 'The location to get the weather forecast for.',
+        },
+        country: {
+          'type': 'string',
+          'description': 'The country of the location.',
+          'x-optional': true,
+        },
+      },
+    })
+    expect(result).toStrictEqual({
+      call,
+      nodeId: 'example',
+      name: 'Weather Forecast',
+      description: 'A tool that can be used to get the weather forecast.',
+      parameters: {
+        type: 'object',
+        additionalProperties: false,
+        required: [
+          'location',
+          'country',
+        ],
+        properties: {
+          location: {
+            type: 'string',
+            description: 'The location to get the weather forecast for.',
+          },
+          country: {
+            'type': 'string',
+            'description': 'The country of the location.',
+            'x-optional': true,
+          },
+        },
+      },
+    })
+  })
+
+  it('should not include extra properties', () => {
+    const call = () => Promise.resolve({ weather: 'sunny' })
+    const result = defineTool('example', {
+      call,
+      name: 'Weather Forecast',
+      description: 'A tool that can be used to get the weather forecast.',
+      properties: {},
+      // @ts-expect-error: extra property
+      extra: 'property',
+    })
+    expect(result).toStrictEqual({
+      call,
+      nodeId: 'example',
+      name: 'Weather Forecast',
+      description: 'A tool that can be used to get the weather forecast.',
+      parameters: {
+        required: [],
+        additionalProperties: false,
+        properties: {},
+        type: 'object',
+      },
+    })
+  })
+})
---
+++ packages/core/utils/toolSchema.ts
@@ -0,0 +1,76 @@
+import type { InferSchema, Schema } from './defineComponent'
+
+/** A tool is a function that can be used by a node to extend it's functionality. */
+export type Tool = InferSchema<typeof toolSchema>
+
+/** The JSON schema of a tool. */
+export const toolSchema = {
+  type: 'object',
+  additionalProperties: false,
+  required: [
+    'name',
+    'description',
+    'parameters',
+    'call',
+  ],
+  properties: {
+    nodeId: {
+      'type': 'string',
+      'title': 'Node ID',
+      'description': 'The ID of the node that the tool is associated with.',
+      'x-internal': true,
+    },
+    name: {
+      type: 'string',
+      title: 'Name',
+      description: 'The name of the tool to identify it in the list of tools.',
+    },
+    description: {
+      type: 'string',
+      title: 'Description',
+      description: 'The description of the tool to provide additional information about it to LLM. It allows the LLM to understand the purpose of the tool and how, when, and where to use it.',
+    },
+    parameters: {
+      type: 'object',
+      title: 'Properties',
+      description: 'The schema of the data that the tool can accept.',
+      required: ['type', 'required', 'properties'],
+      properties: {
+        type: {
+          type: 'string',
+          enum: ['object'] as const,
+          default: 'object',
+        },
+        required: {
+          type: 'array',
+          items: { type: 'string' },
+        },
+        properties: {
+          type: 'object',
+          additionalProperties: { type: 'object', additionalProperties: true },
+        },
+        additionalProperties: {
+          type: 'boolean',
+          default: false,
+        },
+      },
+    },
+    call: {
+      'title': 'Call',
+      'description': 'The function that calls the language model API.',
+      'x-type': 'function',
+      'x-resolves': {
+        type: 'object',
+        additionalProperties: true,
+      },
+      'x-returns': {
+        type: 'object',
+        additionalProperties: true,
+      },
+      'x-takes': [{
+        type: 'object',
+        additionalProperties: true,
+      }],
+    },
+  },
+} satisfies Schema
