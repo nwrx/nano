e7156a0c450ba41a209642f66e348dd164124a0e
Stanley Horwood <stanley.horwood@nanoworks.io>
2025-01-31 19:08:25 +0100

    feat(core): simplify and streamline the `createReference` API
    Significantly refactored the `createReference` function and associated tests in the `core` package to simplify the API and reduce code complexity. Removed multiple specific reference type functions and their corresponding type checks, consolidating them into a single, versatile `createReference` function that accepts a kind and an optional list of values. This change makes the API easier to use and maintain, as it decreases the overall codebase while improving the function's flexibility and readability.

--- packages/core/utils/createReference.ts
+++ packages/core/utils/createReference.ts
@@ -1,39 +1,6 @@
-import type { ObjectLike } from '@unshared/types'
-
-export interface ReferenceSecret {
-  $fromSecret: {
-    name: string
-    defaultValue?: string
-  }
+export interface Reference {
+  $ref: string
 }
-
-export interface ReferenceVariable {
-  $fromVariable: {
-    name: string
-    defaultValue?: string
-  }
-}
-
-export interface ReferenceNode {
-  $fromNode: {
-    id: string
-    name: string
-    path?: string
-  }
-}
-
-export interface ReferenceFile {
-  $fromFile: {
-    path: string
-    defaultValue?: string
-  }
-}
-
-export type Reference =
-  | ReferenceFile
-  | ReferenceNode
-  | ReferenceSecret
-  | ReferenceVariable
 
 /**
  * Create a reference of a given type. The reference can be used to reference
@@ -41,48 +8,17 @@ export type Reference =
  * available at runtime.
  *
  * @param kind The kind of the reference to create.
- * @param options The options to create the reference with.
+ * @param values The values to use to create the reference.
  * @returns The reference created with the given options.
  * @example
  *
  * // Create a reference to a secret value.
- * const secretRef = createReference('fromSecret', {
- *   name: 'MY_SECRET',
- *   path: 'password',
- *   defaultValue: 'default-password',
- * })
+ * const secretRef = createReference('Secrets', 'SECRET_NAME') // { $ref: '#Secrets/SECRET_NAME' }
  *
  * // Create a reference to a node value.
- * const nodeRef = createReference('fromNode', {
- *   id: 'node-id',
- *   key: 'output',
- *   path: 'value',
- * })
+ * const nodeRef = createReference('Node', 'NODE_ID') // { $ref: '#Node/NODE_ID' }
  */
-export function createReference(kind: 'fromFile', options: ReferenceFile['$fromFile']): ReferenceFile
-export function createReference(kind: 'fromNode', options: ReferenceNode['$fromNode']): ReferenceNode
-export function createReference(kind: 'fromVariable', options: ReferenceVariable['$fromVariable']): ReferenceVariable
-export function createReference(kind: 'fromSecret', options: ReferenceSecret['$fromSecret']): ReferenceSecret
-export function createReference(kind: string, options: ObjectLike): Reference {
-  return { [`$${kind}`]: options } as unknown as Reference
-}
-
-export function isReferenceLink(value: unknown): value is ReferenceNode {
-  return typeof value === 'object' && value !== null && '$fromNode' in value
-}
-
-export function isReferenceSecret(value: unknown): value is ReferenceSecret {
-  return typeof value === 'object' && value !== null && '$fromSecret' in value
-}
-
-export function isReferenceVariable(value: unknown): value is ReferenceVariable {
-  return typeof value === 'object' && value !== null && '$fromVariable' in value
-}
-
-export function isReferenceFile(value: unknown): value is ReferenceFile {
-  return typeof value === 'object' && value !== null && '$fromFile' in value
-}
-
-export function isReference(value: unknown): value is Reference {
-  return isReferenceLink(value) || isReferenceSecret(value) || isReferenceVariable(value) || isReferenceFile(value)
+export function createReference(kind: string, ...values: string[]): Reference {
+  const value = [kind, ...values].join('/')
+  return { $ref: `#${value}` }
 }
