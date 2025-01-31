edee6daa520c9756f8b046b6a2ab4fab8e41a5de
Stanley Horwood <stanley.horwood@nanoworks.io>
2025-01-31 19:06:53 +0100

    chore(app): consolidate date formatting utilities and enhance functionality
    Consolidated the date formatting functions into a single file, `formatDate.ts`, and introduced a new function `formatDateTime` to handle date and time formatting. Removed the redundant `formatDateFromNow.ts` file to streamline the codebase. These changes simplify the management of date utilities and extend the formatting capabilities to support both date only and date-time formats. This adjustment not only cleans up the code structure but also improves utility across different components where date formatting is required.

--- packages/app/utils/formatDate.ts
+++ packages/app/utils/formatDate.ts
@@ -1,4 +1,5 @@
 import { format } from 'date-fns/format'
+import { formatDistanceToNow } from 'date-fns/formatDistanceToNow'
 import { de } from 'date-fns/locale/de'
 import { enUS as en } from 'date-fns/locale/en-US'
 import { es } from 'date-fns/locale/es'
@@ -11,9 +12,35 @@ import { zhCN as zh } from 'date-fns/locale/zh-CN'
  * @param date The date to format.
  * @returns The formatted date.
  */
-export function formatDate(date?: string) {
+export function formatDate(date?: Date | number | string): string {
   if (!date) return '-'
   const { locale } = useI18n()
   const localeObject = { de, en, fr, zh, es }[locale.value.split('-')[0]]
   return format(date, 'PP', { locale: localeObject })
+}
+
+/**
+ * Return the formatted date and time according to the current locale.
+ *
+ * @param date The date to format.
+ * @returns The formatted date and time.
+ */
+export function formatDateTime(date?: Date | number | string): string {
+  if (!date) return '-'
+  const { locale } = useI18n()
+  const localeObject = { de, en, fr, zh, es }[locale.value.split('-')[0]]
+  return format(date, 'PPp', { locale: localeObject })
+}
+
+/**
+ * Return the formatted date according to the current locale.
+ *
+ * @param date The date to format.
+ * @returns The formatted date.
+ */
+export function formatDateFromNow(date?: string): string {
+  if (!date) return '-'
+  const { locale } = useI18n()
+  const localeObject = { de, en, fr, zh, es }[locale.value.split('-')[0]]
+  return formatDistanceToNow(date, { addSuffix: true, locale: localeObject })
 }
