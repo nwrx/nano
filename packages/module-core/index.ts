6e28d2aa1aff67a8c99d6388cf7271d7b9dee4dd
Stanley Horwood <stanley.horwood@nanoworks.io>
2025-01-31 19:07:38 +0100

    feat(module-core): autogenerate modules from OpenAPI or Postman collections
    Implemented functionality in the `module-core` package to automatically generate modules from either OpenAPI specifications or Postman collections. This enables easier integration and faster setup for new services by auto-generating nodes, input schemas, and output schemas based on the provided API specifications. The process leverages new utility functions and integrates with existing infrastructure to parse and transform API descriptions into usable module components.

--- packages/module-core/index.ts
+++ packages/module-core/index.ts
@@ -1,10 +1,29 @@
 import { defineModule } from '@nwrx/core'
 import * as nodes from './nodes'
+import { importFromPostmanCollection } from './openapi'
 
 export const Core = defineModule({
   kind: 'core',
   name: 'Core',
   icon: 'https://api.iconify.design/carbon:ibm-cloud-kubernetes-service.svg',
   description: 'Basic nodes and types for building automation flows.',
-  nodes,
+  nodes: async() => ({
+    ...nodes,
+
+    // --- Notion.
+    ...await importFromPostmanCollection('15568543-d990f9b7-98d3-47d3-9131-4866ab9c6df2', {
+      kind: 'notion',
+      name: 'Notion',
+      color: '#4a154b',
+      icon: 'https://api.iconify.design/simple-icons:notion.svg',
+    }),
+
+    // --- Gmail.
+    ...await importFromPostmanCollection('35240-0d073c40-6ad8-43d0-8baf-8ee3606819e0', {
+      kind: 'gmail',
+      name: 'Gmail',
+      color: '#d93025',
+      icon: 'https://api.iconify.design/skill-icons:gmail-light.svg',
+    }),
+  }),
 })
---
+++ packages/module-core/openapi/createOutputSchema.ts
@@ -0,0 +1,33 @@
+/* eslint-disable @typescript-eslint/no-unsafe-member-access */
+import type { OutputSchema } from '@nwrx/core'
+import type { OpenAPI, OpenAPIV2, OpenAPIV3 } from 'openapi-types'
+import { createNodeSocket } from './createNodeSocket'
+
+export function createOutputSchema(operation: OpenAPI.Operation): OutputSchema {
+  const outputSchema: OutputSchema = {}
+
+  // --- Find the first OK response and extract the schema.
+  for (const status in operation.responses) {
+    const code = Number.parseInt(status)
+    if (code < 200 || code >= 300) continue
+
+    // --- Will attempt to access the schema property. Abort if not found.
+    const schema = 'schema' in operation.responses[status]!
+      ? operation.responses[status]?.schema as OpenAPIV2.SchemaObject
+    // @ts-expect-error: will abort if the schema property is not found.
+      : operation.responses[status]?.content?.['application/json']?.schema as OpenAPIV3.SchemaObject
+    if (!schema) continue
+
+    // --- When the schema is an object, we create a separate output for each property.
+    // @ts-expect-error: ignore
+    outputSchema.data = {
+      ...createNodeSocket(schema),
+      name: 'Data',
+      description: 'The output data as an object.',
+    }
+
+    // --- We only process the first OK response.
+    break
+  }
+  return outputSchema
+}
