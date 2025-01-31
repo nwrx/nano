f0a529ded12a58775899db043de180a9af73373e
Stanley Horwood <stanley.horwood@nanoworks.io>
2025-01-31 19:06:27 +0100

    feat(api): add export functionality for flows
    Implemented a new route for exporting flows in YAML format. This feature allows users to download a serialized version of their flow configurations directly from the API. The route handles user authentication and permissions, ensuring that only authorized users can export flows. Added necessary headers to the response to facilitate the download as a YAML file. This enhancement improves the usability and flexibility of the flow management system.

---
+++ packages/api/flow/routes/flowExport.ts
@@ -0,0 +1,37 @@
+import type { ModuleFlow } from '..'
+import { ModuleUser } from '@nwrx/api/user'
+import { ModuleWorkspace } from '@nwrx/api/workspace'
+import { createRoute } from '@unserved/server'
+import { assertStringNotEmpty, createSchema } from '@unshared/validation'
+import { setResponseHeader } from 'h3'
+import * as YAML from 'yaml'
+
+export function flowExport(this: ModuleFlow) {
+  return createRoute(
+    {
+      name: 'GET /api/workspaces/:workspace/:project/:flow/export',
+      parameters: createSchema({
+        workspace: assertStringNotEmpty,
+        project: assertStringNotEmpty,
+        flow: assertStringNotEmpty,
+      }),
+    },
+    async({ event, parameters }) => {
+      const userModule = this.getModule(ModuleUser)
+      const workspaceModule = this.getModule(ModuleWorkspace)
+      const { user } = await userModule.authenticate(event)
+      const { workspace: workspaceName, project: projectName, flow: flowName } = parameters
+
+      // --- Resolve the flow and check if the user has access to it.
+      const workspace = await workspaceModule.resolveWorkspace({ user, name: workspaceName, permission: 'Read' })
+      const project = await workspaceModule.resolveProject({ workspace, name: projectName, permission: 'Read' })
+      const flow = await this.resolveFlow({ name: flowName, project, workspace })
+
+      // --- Return the serialized flow.
+      const data = YAML.stringify(flow.data)
+      setResponseHeader(event, 'Content-Type', 'application/yaml')
+      setResponseHeader(event, 'Content-Disposition', `attachment; filename="${flowName}.yaml"`)
+      return data
+    },
+  )
+}
