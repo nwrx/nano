3bf8e275379249b48d6a372fa61136bceeb2acfb
Stanley Horwood <stanley.horwood@nanoworks.io>
2025-01-31 19:06:23 +0100

    chore(app): add slot usage in navigation components
    Refactored the `AppPageNav` and `AppPageNav.Group` Vue components to use slots for rendering items and groups. This change improves the flexibility of these components, allowing for more customized content injection by parent components. The padding in `AppPageNav` was also adjusted to ensure consistent spacing.

--- packages/app/components/app/AppPageNav.vue
+++ packages/app/components/app/AppPageNav.vue
@@ -5,15 +5,18 @@ defineProps<{ groups: NavItem[] }>()
 <template>
   <div
     class="
-      flex flex-col w-full h-full max-w-80 gap-2 pr-6
+      flex flex-col w-full h-full max-w-80 gap-2 pr-8
       bg-white border-r border-black/10
     ">
 
-    <AppPageNavGroup
-      v-for="group in groups"
-      :key="group.label"
-      :label="group.label"
-      :items="group.items"
-    />
+    <!-- Content -->
+    <slot>
+      <AppPageNavGroup
+        v-for="group in groups"
+        :key="group.label"
+        :label="group.label"
+        :items="group.items"
+      />
+    </slot>
   </div>
 </template>
