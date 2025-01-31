d059058dbb376f6856ee2eba17dee9ea51603bb1
Stanley Horwood <stanley.horwood@nanoworks.io>
2025-01-31 19:06:08 +0100

    chore(app): ensure full height in `AppPage.Container` and `AppPageNav` components
    Adjusted CSS classes in both `AppPage.Container.vue` and `AppPageNav.vue` to ensure they utilize the full height of their parent containers. This change is part of ongoing efforts to standardize layout behaviors across components, enhancing the visual consistency and usability of the UI.

--- packages/app/components/app/AppPage.Container.vue
+++ packages/app/components/app/AppPage.Container.vue
@@ -6,7 +6,7 @@ defineProps<{
 
 <template>
   <div
-    class="flex flex-col sm:p-16 p-8 items-center w-full"
+    class="flex flex-col sm:p-16 p-8 items-center w-full h-full"
     :class="{ 'max-w-container mx-auto': contained }">
     <slot/>
   </div>
