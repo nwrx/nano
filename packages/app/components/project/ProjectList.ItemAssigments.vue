6c86613c83b0152776f9c86e7f0f5f9ef9070b36
Stanley Horwood <stanley.horwood@nanoworks.io>
2025-01-31 19:06:34 +0100

    chore(app): update UI theming and styling across project components
    Updated various UI elements across the project components to enhance consistency and adhere to the new theming guidelines. Changes include adjustments to padding, border styles, icon sizes, and text classes to improve visual hierarchy and readability. This ensures a more cohesive user experience across the platform.

--- packages/app/components/project/ProjectList.ItemAssigments.vue
+++ packages/app/components/project/ProjectList.ItemAssigments.vue
@@ -7,11 +7,11 @@ defineProps<{
 </script>
 
 <template>
-  <div class="flex items-center justify-center -space-x-2">
+  <div class="flex items-center justify-center -space-x-sm">
     <div
       v-for="assignment in assignments"
       :key="assignment.user.username"
-      class="w-8 h-8 rounded-full border-1 border-black/10 bg-center bg-cover bg-no-repeat"
+      class="size-8 rounded-full border border-app-active bg-center bg-cover bg-no-repeat"
       :style="{ backgroundImage: `url(${assignment.user.avatarUrl})` }"
     />
   </div>
