de316b8a18bf06bcd14b23ad46b9a81e17c3f5c0
Stanley Horwood <stanley.horwood@nanoworks.io>
2025-01-31 19:08:24 +0100

    chore(app): update AppPage.Header component and add AppPage.HeaderTab
    Updated the AppPage.Header component to support both single and multiple titles using the MaybeArray type. This enhancement improves the flexibility of the header component by allowing it to accommodate various title formats seamlessly. Additionally, the layout of the component has been modified to be sticky at the top with a new styling scheme that enhances its visibility and functionality within the application interface.
    
    Introduced a new component, AppPage.HeaderTab, which utilizes the BaseButtonProps for better interactivity and consistency across different parts of the application. This component is designed to be used within the AppPage.Header for better navigation and user experience, providing a clear and interactive way to switch between different views or sections.

---
+++ packages/app/components/app/AppPage.HeaderTab.vue
@@ -0,0 +1,36 @@
+<!-- eslint-disable vue/prop-name-casing -->
+<script setup lang="ts">
+import type { BaseButtonProps } from '@unshared/vue'
+
+const props = defineProps<BaseButtonProps & {
+  icon?: string
+}>()
+</script>
+
+<template>
+  <BaseButton
+    v-bind="props"
+    eager
+    class-active="
+      !bg-active !text-active font-medium
+    "
+    class="
+      flex items-center gap-sm cursor-pointer
+      text-app rd-t px-sm py-xs
+      hover:bg-prominent
+    ">
+
+    <!-- Icon -->
+    <BaseIcon
+      v-if="icon"
+      :icon="icon"
+      class="size-4"
+    />
+
+    <!-- Label -->
+    <span
+      class="text-base font-medium"
+      v-text="label"
+    />
+  </BaseButton>
+</template>
