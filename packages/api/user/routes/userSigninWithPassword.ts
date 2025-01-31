63ab54f176b1625daf48db5abb1a4fb1f91266f2
Stanley Horwood <stanley.horwood@nanoworks.io>
2025-01-31 19:05:59 +0100

    chore(api): streamline session logic and improve code readability
    Significant modifications were made to the session handling logic in both the `userSigninWithPassword` and `userSignupWithPassword` routes. These changes include the removal of unnecessary parameters and the consolidation of session creation steps into the `createSession` utility function. This refactor enhances code readability and maintains functionality by centralizing session creation, which now uniformly handles user session and token generation. The update ensures that all session-related actions are consistently managed across different parts of the application, reducing redundancy and potential errors.

--- packages/api/user/routes/userSigninWithPassword.ts
+++ packages/api/user/routes/userSigninWithPassword.ts
@@ -1,21 +1,19 @@
 import type { ModuleUser } from '../index'
 import { createRoute } from '@unserved/server'
 import { assertStringNotEmpty, createSchema } from '@unshared/validation'
-import { getHeader, getRequestIP, setCookie } from 'h3'
+import { setCookie } from 'h3'
 
 export function userSigninWithPassword(this: ModuleUser) {
   return createRoute(
     {
-      name: 'POST /api/signin',
+      name: 'POST /api/session',
       body: createSchema({
         username: assertStringNotEmpty,
         password: assertStringNotEmpty,
       }),
     },
-
-    async({ event, body }) => {
+    async({ event, body }): Promise<void> => {
       const { username, password } = body
-      const { User, UserSession } = this.getRepositories()
 
       // --- Try authenticating the user from the Cookie.
       // --- If successful, do not create a new session.
@@ -23,29 +21,27 @@ export function userSigninWithPassword(this: ModuleUser) {
       if (isSignedIn) return
 
       // --- Get the user by the username and check the password.
-      const user = await User.findOne({ where: [
-        { username },
-        { email: username },
-      ] })
+      const { User } = this.getRepositories()
+      const user = await User.findOne({ where: [{ username }, { email: username }] })
+      if (!user) throw this.errors.USER_BAD_CREDENTIALS()
+      if (user.disabledAt) throw this.errors.USER_DISABLED()
+      if (user.verifiedAt === null) throw this.errors.USER_NOT_VERIFIED()
 
-      // --- Throw an error if the user is not found or the password is incorrect.
-      if (!user) return this.errors.USER_BAD_CREDENTIALS()
-      const isPasswordCorrect = await user.checkPassword(password)
-      if (!isPasswordCorrect) return this.errors.USER_BAD_CREDENTIALS()
+      // --- Find and check the user's password.
+      const isPasswordCorrect = await this.checkPassword(user, password)
+      if (!isPasswordCorrect) throw this.errors.USER_BAD_CREDENTIALS()
 
       // --- Create a session for the user.
-      const address = getRequestIP(event, { xForwardedFor: this.userTrustProxy })
-      const userAgent = getHeader(event, 'User-Agent')
-      const userSession = this.createSession(user, { address, userAgent })
-      const token = this.createSessionToken(userSession)
-      await UserSession.save(userSession)
+      const { UserSession } = this.getRepositories()
+      const { session, token } = this.createSession(event, { user })
+      await UserSession.save(session)
 
       // --- Set the response status, content type, and user session cookie.
       setCookie(event, this.userSessionCookieName, token, {
         secure: true,
         httpOnly: true,
         sameSite: 'strict',
-        maxAge: (userSession.expiresAt.getTime() - Date.now()) / 1000,
+        maxAge: (session.expiresAt.getTime() - Date.now()) / 1000,
       })
     },
   )
--- packages/api/user/utils/createSession.ts
+++ packages/api/user/utils/createSession.ts
@@ -1,18 +1,25 @@
 import type { User } from '../entities'
 import type { UserSession } from '../entities'
 import type { ModuleUser } from '../index'
+import { getHeader, getRequestIP, type H3Event } from 'h3'
 import { createCipheriv, createHash } from 'node:crypto'
 
 export interface CreateSessionOptions {
 
-  /** The IP address to assign to the session. */
-  address?: string
-
-  /** The user agent to assign to the session. */
-  userAgent?: string
+  /** The user to create the session for. */
+  user: User
 
   /** The duration of the session in milliseconds. */
   duration?: number
+}
+
+export interface CreateSessionResult {
+
+  /** The created user session. */
+  session: UserSession
+
+  /** The token for the session. */
+  token: string
 }
 
 /**
@@ -20,25 +27,21 @@ export interface CreateSessionOptions {
  * and authorize the user to access the resources on the server. A session is
  * assigned to a single address and user agent and expires after a certain time.
  *
- * @param user The user to create the session for.
+ * @param event The event that triggered the session creation.
  * @param options The options to create the session with.
  * @returns The user session.
  */
-export function createSession(this: ModuleUser, user: User, options: CreateSessionOptions): { session: UserSession; token: string } {
+export function createSession(this: ModuleUser, event: H3Event, options: CreateSessionOptions): CreateSessionResult {
   const { UserSession } = this.getRepositories()
-  const { address, userAgent, duration = this.userSessionDuration } = options
+  const { user, duration = this.userSessionDuration } = options
 
   // --- Get the IP address and user agent.
-  if (!address) throw this.errors.USER_MISSING_HEADER()
-  if (!userAgent) throw this.errors.USER_MISSING_HEADER()
+  const address = getRequestIP(event, { xForwardedFor: this.userTrustProxy })
+  const userAgent = getHeader(event, 'User-Agent')
+  const expiresAt = new Date(Date.now() + duration)
 
   // --- Create the session entity.
-  const session = UserSession.create({
-    user,
-    address,
-    userAgent,
-    expiresAt: new Date(Date.now() + duration),
-  })
+  const session = UserSession.create({ user, address, userAgent, expiresAt })
 
   // --- Create the token for the session.
   const iv = Buffer.alloc(16, 0)
