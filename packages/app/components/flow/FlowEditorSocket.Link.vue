d9ddfd7db47726cc303972bf738bd639555ca16a
Stanley Horwood <stanley.horwood@nanoworks.io>
2025-01-31 19:06:52 +0100

    chore(app): extract display logic into `FlowEditorSocket.Link` component
    Refactored the `FlowEditorSocket.vue` component to extract the display logic for linkable pins into a new component, `FlowEditorSocket.Link.vue`. This change simplifies the main component by delegating the rendering of linkable pin details to the new component, improving modularity and maintainability of the code. The new component handles the display of the pin's name and default value when it is not linked, enhancing the separation of concerns within the application's components.

---
+++ packages/app/components/flow/FlowEditorSocket.Link.vue
@@ -0,0 +1,25 @@
+<script setup lang="ts">
+defineProps<{
+  name: string
+  isLinked: boolean
+  defaultValue: string
+}>()
+</script>
+
+<template>
+  <div class="truncate px-sm py-xs">
+
+    <!-- Name -->
+    <span
+      class="text-sm"
+      v-text="name"
+    />
+
+    <!-- Default -->
+    <span
+      v-if="!isLinked"
+      class="text-subtle text-xs"
+      v-text="defaultValue"
+    />
+  </div>
+</template>
