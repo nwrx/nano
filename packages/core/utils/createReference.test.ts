b357b3785333e92b791ce5c9cd475e633e2dbaf2
Stanley Horwood <stanley.horwood@nanoworks.io>
2025-01-31 19:08:25 +0100

    chore(core): standardize and enhance `createReference` function and tests
    Updated the `createReference` function in the `core` package to use stricter typing, ensuring that the `kind` parameter conforms to predefined types only. This change improves the robustness and predictability of reference creation by enforcing type constraints. Additionally, updated the corresponding unit tests to use `toStrictEqual` for comparisons, providing more accurate test validations by checking the type and value structure in a stricter manner.

--- packages/core/utils/createReference.test.ts
+++ packages/core/utils/createReference.test.ts
@@ -3,16 +3,16 @@ import { createReference } from './createReference'
 describe('createReference', () => {
   it('should create a reference with no values', () => {
     const result = createReference('Memory')
-    expect(result).toEqual({ $ref: '#Memory' })
+    expect(result).toStrictEqual({ $ref: '#Memory' })
   })
 
   it('should create a reference with a single value', () => {
     const result = createReference('Node', 'NODE_ID')
-    expect(result).toEqual({ $ref: '#Node/NODE_ID' })
+    expect(result).toStrictEqual({ $ref: '#Node/NODE_ID' })
   })
 
   it('should create a reference with multiple values', () => {
     const result = createReference('Node', 'NODE_ID', 'CHILD_ID')
-    expect(result).toEqual({ $ref: '#Node/NODE_ID/CHILD_ID' })
+    expect(result).toStrictEqual({ $ref: '#Node/NODE_ID/CHILD_ID' })
   })
 })
