8a05be3efa2d43aa378a33c96aefffe55ea63b2a
Stanley Horwood <stanley.horwood@nanoworks.io>
2025-01-31 19:06:27 +0100

    feat(api): introduce `FlowSession` and `FlowSessionEvent` entities
    Replaced `FlowRun` and `FlowRunEvent` with new entities `FlowSession` and `FlowSessionEvent` to enhance logging and tracking of flow executions. This change provides a more structured and detailed logging system, capturing specific events and their data during the flow sessions. This structural shift aims to improve the accuracy and the granularity of the data captured during flow executions, making it easier to debug and analyze workflows.

---
+++ packages/api/flow/entities/FlowSession.ts
@@ -0,0 +1,48 @@
+import { BaseEntity } from '@unserved/server'
+import { Entity, JoinColumn, ManyToOne, OneToMany } from 'typeorm'
+import { UserObject } from '../../user'
+import { Flow, FlowObject } from './Flow'
+import { FlowSessionEvent, FlowSessionEventObject } from './FlowSessionEvent'
+
+/**
+ * A `FlowSession` is used to log the execution of a flow. It is used to store the
+ * information about the flow run such as the start time, end time, status, etc.
+ */
+@Entity({ name: 'FlowSession' })
+export class FlowSession extends BaseEntity {
+
+  /**
+   * The flow that was executed.
+   *
+   * @example Flow { ... }
+   */
+  @JoinColumn()
+  @ManyToOne(() => Flow, { onDelete: 'CASCADE' })
+  flow: Flow
+
+  /**
+   * The events that occurred during the flow run.
+   *
+   * @example [FlowSessionEvent, FlowSessionEvent, FlowSessionEvent]
+   */
+  @OneToMany(() => FlowSessionEvent, event => event.session, { cascade: true })
+  events?: FlowSessionEvent[]
+
+  /**
+   * @returns The object representation of the flow run.
+   */
+  serialize(): FlowSessionObject {
+    return {
+      id: this.id,
+      flow: this.flow.serialize(),
+      events: this.events?.map(event => event.serialize()),
+    }
+  }
+}
+
+export interface FlowSessionObject {
+  id: string
+  flow: FlowObject
+  user?: UserObject
+  events?: FlowSessionEventObject[]
+}
