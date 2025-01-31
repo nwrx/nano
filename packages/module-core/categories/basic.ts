15d89b27b92b472dc308a49fff1abf3f7bf8a6c4
Stanley Horwood <stanley.horwood@nanoworks.io>
2025-01-31 19:06:39 +0100

    chore(module-core): update color codes for category definitions
    Updated the color codes for the `basic`, `compute`, and `languageModel` categories to better align with the visual design guidelines. These changes improve the visual cohesion across the platform's UI, aiding in user navigation and category recognition.

--- packages/module-core/categories/basic.ts
+++ packages/module-core/categories/basic.ts
@@ -3,7 +3,7 @@ import { defineCategory } from '@nwrx/core'
 export const basic = defineCategory({
   kind: 'basic',
   name: 'Basic',
-  color: '#E3B505',
+  color: '#AFD7A5',
   icon: 'https://api.iconify.design/carbon:assembly.svg',
   description: 'A collection of basic nodes for performing simple operations.',
 })
--- packages/module-core/categories/languageModel.ts
+++ packages/module-core/categories/languageModel.ts
@@ -3,7 +3,7 @@ import { defineCategory } from '@nwrx/core'
 export const languageModel = defineCategory({
   kind: 'languageModel',
   name: 'Language Models',
-  color: '#160D45',
+  color: '#5847A8',
   icon: 'https://api.iconify.design/carbon:model.svg',
   description: 'A collection of nodes for working with language models.',
 })
