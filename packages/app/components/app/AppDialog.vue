9b877429f3e158f2f16b69097de7905b6153a4ef
Stanley Horwood <stanley.horwood@nanoworks.io>
2025-01-31 19:06:22 +0100

    chore(app): refactor the AppDialog component
    Refactored the AppDialog component to improve modularity and maintainability. Introduced new child components for different sections of the dialog, such as AppDialog.Header and AppDialog.Text, to simplify the overall structure and enhance readability. Additionally, removed the AppPage.Hint component and integrated its functionalities where necessary within the dialog components. This change streamlines the dialog's implementation and aligns with best practices for component-based architecture.

--- packages/app/components/app/AppDialog.vue
+++ packages/app/components/app/AppDialog.vue
@@ -1,73 +1,94 @@
+<i18n lang="yaml">
+en:
+  cancel: Cancel
+  confirm: Confirm
+fr:
+  cancel: Annuler
+  confirm: Confirmer
+de:
+  cancel: Abbrechen
+  confirm: Bestätigen
+cn:
+  cancel: 取消
+  confirm: 确认
+</i18n>
+
 <script setup lang="ts">
 import type { BaseDialogProps } from '@unshared/vue'
 
 const props = defineProps<{
   title?: string
   text?: string
+  icon?: string
+  variant?: Variant
+  labelConfirm?: string
+  labelCancel?: string
 } & BaseDialogProps>()
 
-defineOptions({
-  inheritAttrs: false,
-})
+const { t } = useI18n({ useScope: 'local' })
 </script>
 
 <template>
-  <Teleport to="#app" defer>
+  <Teleport to="#layout" defer>
     <LazyBaseDialog
       v-bind="props"
       :class="{
-        'backdrop-contrast-80': props.modelValue,
-        'pointer-events-none': !props.modelValue,
+        'backdrop-brightness-40': modelValue,
+        'pointer-events-none': !modelValue,
       }"
       class="
-          fixed left-0 top-0 z-50 w-full h-full scale-105 bg-transparent
-          inline-flex items-center justify-center backdrop:bg-transparent
-          transition-all duration-200 p-0
-        ">
+        fixed left-0 top-0 z-50 w-full h-full scale-105 bg-transparent
+        inline-flex items-center justify-center backdrop:bg-transparent
+        transition-all duration-200 p-0
+      ">
 
       <!-- Content -->
       <template #dialog="slots">
         <div
-
           class="
-              rounded max-w-xl pointer-events-auto
-              bg-white dark:bg-primary-900
-              border border-primary-500/20 shadow-2xl shadow-black/10
-              transition-all duration-200
-            "
+            rounded-lg max-w-xl pointer-events-auto
+            bg-white dark:bg-primary-900
+            text-black dark:text-white
+            transition-all duration-100
+          "
           :class="{
             'scale-100 opacity-100 pointer-events-auto': slots.isOpen,
             'scale-95 opacity-0 pointer-events-none': !slots.isOpen,
           }">
 
           <!-- Title -->
-          <div class="flex items-start justify-between border-b border-black/10 p-4">
-            <div class="-space-y-1">
-              <h2 class="text-base font-medium">
-                <slot name="title">{{ props.title }}</slot>
-              </h2>
-              <p class="text-sm opacity-60">
-                <slot name="text">{{ props.text }}</slot>
-              </p>
-            </div>
-            <Button
-              eager
-              light
-              variant="primary"
-              icon="i-carbon:close"
-              class="!p-1"
-              @click="() => slots.close()"
-            />
-          </div>
+          <AppDialogHeader
+            :title="title"
+            :isOpen="slots.isOpen"
+            @close="() => slots.close()">
+            <slot name="title" />
+          </AppDialogHeader>
+
+          <!-- Hint -->
+          <AppDialogText
+            v-if="text || $slots.text"
+            :text="text"
+            :icon="icon"
+            :variant="variant">
+            <slot name="text" />
+          </AppDialogText>
 
           <!-- Dialog content -->
-          <div class="p-8 w-full" v-bind="$attrs">
+          <div class="p-4 w-full">
             <slot v-bind="slots" />
           </div>
 
           <!-- CTA -->
-          <div v-if="$slots.cta" class="p-4 w-full bg-black/5">
-            <slot name="cta" v-bind="slots" />
+          <div class="p-4 w-full bg-primary-500/10">
+            <slot name="actions" v-bind="slots">
+              <AppDialogActions
+                :labelCancel="labelCancel ?? t('cancel')"
+                :labelConfirm="labelConfirm ?? t('confirm')"
+                :variant="variant"
+                @close="slots.close"
+                @confirm="() => slots.returnValue(true)"
+              />
+            </slot>
           </div>
         </div>
       </template>
