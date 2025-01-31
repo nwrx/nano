dca5db2665128b68ba727536c112aa021ecc7357
Pierre SARAZIN <pierre.sarazin@nanoworks.io>
2025-01-31 19:07:15 +0100

    feat(app): add tooltip component

--- packages/app/components/editor/Editor.Toolbar.vue
+++ packages/app/components/editor/Editor.Toolbar.vue
@@ -11,6 +11,8 @@ const emit = defineEmits<{
   bookmark: []
   download: []
 }>()
+
+const { t } = useI18n()
 </script>
 
 <template>
@@ -38,10 +40,29 @@ const emit = defineEmits<{
     />
 
     <!-- Start -->
-    <EditorFab
-      :is-active="isRunning"
-      :icon="isRunning ? 'i-line-md:loading-loop' : 'i-carbon:play'"
-      @click="() => isRunning ? emit('abort') : emit('start')"
-    />
+    <Tooltip
+      :message="t('startFlow')"
+      size="md"
+      position="bottom"
+      variant="primary">
+      <EditorFab
+        :is-active="isRunning"
+        :icon="isRunning ? 'i-line-md:loading-loop' : 'i-carbon:play'"
+        @click="() => isRunning ? emit('abort') : emit('start')"
+      />
+    </Tooltip>
   </div>
 </template>
+
+<i18n lang="yaml">
+en:
+  startFlow: Start the flow
+fr:
+  startFlow: Activer le flux
+de:
+  startFlow: Starten Sie den Fluss
+es:
+  startFlow: Iniciar el flujo
+zh:
+  startFlow: 启动流程
+</i18n>
