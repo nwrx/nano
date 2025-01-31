a1f08c08e0b00b6d30ebdb1ec926fcf19a7f568b
Stanley Horwood <stanley.horwood@nanoworks.io>
2025-01-31 19:06:46 +0100

    chore(monorepo): update patch dependencies and script enhancements
    Updated the `package.json` to include a new patched dependency for `@unshared/validation`. This ensures that our patched dependencies are up-to-date with the latest changes required by our codebase. Additionally, the `patch.sh` script has been improved for better path handling and maintenance of patches. This update involves more robust path resolution and a cleaner approach to removing old patches and applying new ones, enhancing the reliability of our development operations.

--- scripts/patch.sh
+++ scripts/patch.sh
@@ -1,11 +1,16 @@
 #!/bin/bash
 
-# This script is used to patch the distribution of a dependency.
+SCRIPT_DIR=$(realpath $(dirname $0))
 PROJECT=$1
 NAME=$2
-PATCH_DIR=./node_modules/.pnpm_patches/@$PROJECT/$NAME
 
+# Resolve the absolute paths.
+ROOT_DIR=$(realpath $SCRIPT_DIR/..)
+PATCH_DIR=$ROOT_DIR/node_modules/.pnpm_patches/@$PROJECT/$NAME
+TARGET_DIR=$(realpath $ROOT_DIR/../../$PROJECT/packages/$NAME/dist)
+
+# Remove the existing patch and create a new one.
 rm -rf $PATCH_DIR
 pnpm patch @$PROJECT/$NAME --ignore-existing --edit-dir $PATCH_DIR
-cp -r ../../$PROJECT/packages/$NAME/dist $PATCH_DIR
+cp -r $TARGET_DIR $PATCH_DIR
 pnpm patch-commit $PATCH_DIR
