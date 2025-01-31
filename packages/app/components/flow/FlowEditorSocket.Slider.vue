46f004def43f7d49df8f05800b9294c4a06720e2
Stanley Horwood <stanley.horwood@nanoworks.io>
2025-01-31 19:06:52 +0100

    feat(app): add support for default values in FlowEditorSocket.Slider
    Enhanced the `FlowEditorSocket.Slider` component to support default values. This change allows users to specify a default value that is used when no explicit `modelValue` is provided, improving usability and flexibility of the slider component. The update also ensures that the computed value respects the minimum and maximum boundaries, preventing invalid slider positions.

--- packages/app/components/flow/FlowEditorSocket.Slider.vue
+++ packages/app/components/flow/FlowEditorSocket.Slider.vue
@@ -1,7 +1,9 @@
+<!-- eslint-disable vue/no-setup-props-reactivity-loss -->
 <script setup lang="ts">
 const props = defineProps<{
-  name: string
-  modelValue: number
+  name?: string
+  modelValue?: number | string
+  defaultValue?: unknown
   min?: number
   max?: number
   step?: number
@@ -35,39 +37,55 @@ function handleSliderChange(event: MouseEvent) {
   model.value = Math.round(valueStepped * 100) / 100
 }
 
+const value = computed(() => {
+  const { min = 0, max = 100 } = props
+  let value = model.value
+  if (!value) value = typeof props.defaultValue === 'number' ? props.defaultValue : min
+  if (typeof value === 'string') value = Number.parseFloat(value)
+  if (value < min) value = min
+  if (value > max) value = max
+  return value
+})
+
 const width = computed(() => {
   const { min = 0, max = 100 } = props
-  const percentage = (model.value - min) / (max - min) * 100
+  const percentage = (value.value - min) / (max - min) * 100
   return `${percentage}%`
 })
 </script>
 
 <template>
-  <div ref="input" class="flex items-center cursor-pointer w-full h-8 px-sm text-sm">
+  <div ref="input" class="flex items-center cursor-pointer w-full h-8 px-sm text-sm group">
 
     <!-- Bar -->
     <div
-      class="w-full h-7 rd bg-prominent overflow-hidden relative"
+      class="w-full h-6 rd bg-prominent overflow-hidden relative b b-transparent group-hover:b-primary-500"
       @mousedown="(event) => { isDragging = true; handleSliderChange(event) }"
       @mouseup="() => isDragging = false"
-      @mouseleave="() => isDragging = false"
       @mousemove="(event) => handleSliderChange(event)">
 
       <!-- Slider -->
       <div class="w-full h-full pointer-events-none">
         <div
-          class="absolute px-sm w-full h-full flex items-center justify-between bg-primary-500 text-white"
-          :style="{ maskImage: `linear-gradient(to right, white ${width}, transparent ${width})` }">
-          <span v-text="name" />
-          <span v-text="model" />
+          class="
+            absolute px-sm w-full h-full transition
+            flex items-center justify-between
+            bg-primary-500/80 text-white
+            group-hover:bg-primary-500
+          "
+          :style="{
+            maskImage: `linear-gradient(to right, white ${width}, transparent ${width})`,
+          }">
+          <span>{{ name }}</span>
+          <span :class="{ 'op-20': model === undefined }">{{ value }}</span>
         </div>
 
         <!-- Background -->
         <div
           class="absolute px-sm w-full h-full flex items-center justify-between"
           :style="{ maskImage: `linear-gradient(to right, transparent ${width}, white ${width})` }">
-          <span v-text="name" />
-          <span v-text="model" />
+          <span>{{ name }}</span>
+          <span :class="{ 'op-20': model === undefined }">{{ value }}</span>
         </div>
       </div>
     </div>
