6c927294f1e8cb67441c958e992df7c93561657d
Stanley Horwood <stanley@hsjm.io>
2025-01-31 19:05:41 +0100

    chore(config): update ESLint configuration for consistent type imports
    Updated the ESLint configuration to disable the rule '@typescript-eslint/consistent-type-imports' across all files. This change involves restructuring the `eslint.config.mjs` to use an array format allowing for easier future modifications and specific rule overrides per file pattern.
    
    This adjustment ensures greater flexibility in managing imports and enhances compatibility with various TypeScript configurations used across projects.

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,7 +1,13 @@
 import unsharedConfig from '@unshared/eslint-config'
 
-export default unsharedConfig({
-  tsConfigPath: [
-    './tsconfig.json',
-  ],
-})
+export default [
+  ...unsharedConfig({
+    tsConfigPath: ['./tsconfig.json'],
+  }),
+  {
+    files: ['**/*'],
+    rules: {
+      '@typescript-eslint/consistent-type-imports': 'off',
+    },
+  },
+]
