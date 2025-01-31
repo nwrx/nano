3e96f7a272c14de1eefd9f06113ca0c125194fdf
Stanley Horwood <stanley.horwood@nanoworks.io>
2025-01-31 19:08:31 +0100

    feat(core): move trust flag from node options to component definition
    The trust flag, which determines if a node runs in a sandboxed environment, has been moved from the `NodeOptions` to the `Component` definition. This change centralizes the trust setting to the component level, making it more manageable and reducing the risk of unsafe configurations. This adjustment ensures that the trust setting aligns more closely with the component's characteristics rather than its usage context.

--- packages/core/thread/startNode.ts
+++ packages/core/thread/startNode.ts
@@ -1,43 +1,39 @@
-import type { ProcessContext } from '../utils'
+import type { ObjectLike } from '@unshared/types'
 import type { Thread } from './createThread'
 import { processInSandbox } from '../sandbox/processInSandbox'
-import { createEventMetadata } from './createEventMetadata'
+import { createEventMetadata, createProcessContext } from '../utils'
 import { getNode } from './getNode'
 import { getNodeComponent } from './getNodeComponent'
-import { getNodeData } from './getNodeData'
 
-export async function startNode(thread: Thread, id: string) {
-  const node = getNode(thread, id)
-  const component = await getNodeComponent(thread, id)
+export async function startNode(thread: Thread, nodeId: string, data?: ObjectLike): Promise<ObjectLike> {
+  const node = getNode(thread, nodeId)
+  const component = await getNodeComponent(thread, nodeId)
   try {
     node.startedAt = Date.now()
-    node.state = 'processing'
-    thread.dispatch('nodeState', id, node, createEventMetadata(thread, node))
+    node.state = 'starting'
+    thread.dispatch('nodeState', nodeId, createEventMetadata(thread, node))
 
-    // --- Resolve the data by parsing the input based on the schema. Additionally, we
-    // --- will resolve the references in the data by calling the reference resolvers.
-    const context: ProcessContext = {
-      data: await getNodeData(thread, id),
-      trace: payload => thread.dispatch('nodeTrace', id, node, payload, createEventMetadata(thread, node)),
-    }
-
+    // --- Process the node if it has a process method.
     if (component.process) {
-      thread.dispatch('nodeStart', id, node, context.data, createEventMetadata(thread, node))
-      node.result = node.isSandboxed
-        ? await processInSandbox(component.process, { ...context, abortSignal: thread.abortController.signal })
-        : await component.process(context)
+      node.state = 'processing'
+      const context = await createProcessContext(thread, nodeId, data)
+      thread.dispatch('nodeState', nodeId, createEventMetadata(thread, node))
+      thread.dispatch('nodeStart', nodeId, context.data, createEventMetadata(thread, node))
+      node.result = component.isTrusted
+        ? await component.process(context)
+        : await processInSandbox(component.process, context)
     }
 
     node.state = 'done'
-    thread.dispatch('nodeState', id, node, createEventMetadata(thread, node))
-    thread.dispatch('nodeDone', id, node, node.result, createEventMetadata(thread, node))
+    thread.dispatch('nodeState', nodeId, createEventMetadata(thread, node))
+    thread.dispatch('nodeDone', nodeId, node.result, createEventMetadata(thread, node))
     return node.result
   }
   catch (error) {
     node.error = error as Error
     node.state = 'error'
-    thread.dispatch('nodeState', id, node, createEventMetadata(thread, node))
-    thread.dispatch('nodeError', id, node, error as Error, createEventMetadata(thread, node))
+    thread.dispatch('nodeState', nodeId, createEventMetadata(thread, node))
+    thread.dispatch('nodeError', nodeId, error as Error, createEventMetadata(thread, node))
     throw error
   }
 }
