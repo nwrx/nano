d0435433813809041cdf599283f8b12ed39796d7
Stanley Horwood <stanley.horwood@nanoworks.io>
2025-01-31 19:05:58 +0100

    feat(app): conditional navigation paths based on user session
    Updated the navigation drawer to dynamically adjust paths based on the user's session information. This approach uses the `username` from the session state to customize the 'Workspace' navigation item, making the navigation experience more personalized and relevant to the user. This change ensures that the navigation structure adapts to the user context, enhancing usability and user interaction within the application.

--- packages/app/utils/navigation.ts
+++ packages/app/utils/navigation.ts
@@ -1,69 +1,72 @@
-export const NAV_DRAWER_START = computed<NavItem[]>(() => [
-  {
-    items: [
-      {
-        to: '/',
-        label: 'Overview',
-        icon: 'i-carbon:home',
-      },
-      {
-        to: { name: 'Workspace', params: { workspace: 'me' } },
-        label: 'Workspace',
-        icon: 'i-carbon:flow',
-      },
-      {
-        to: '/modules',
-        label: 'Modules',
-        icon: 'i-carbon:box',
-      },
-      {
-        to: '/integrations',
-        label: 'Integrations',
-        icon: 'i-carbon:integration',
-      },
-      {
-        to: '/storage',
-        label: 'Storage',
-        icon: 'i-carbon:db2-database',
-      },
-      {
-        to: '/keys',
-        label: 'API Keys',
-        icon: 'i-carbon:api-key',
-      },
-    ],
-  },
-  {
-    label: 'Organization',
-    items: [
-      {
-        to: '/members',
-        label: 'Members',
-        icon: 'i-carbon:user',
-      },
-      {
-        to: '/settings',
-        label: 'Settings',
-        icon: 'i-carbon:settings',
-      },
-      {
-        to: '/monitoring',
-        label: 'Monitoring',
-        icon: 'i-carbon:cloud-monitoring',
-      },
-      {
-        to: '/usage',
-        label: 'Usage',
-        icon: 'i-carbon:analytics',
-      },
-      {
-        to: '/billing',
-        label: 'Billing',
-        icon: 'i-carbon:document',
-      },
-    ],
-  },
-])
+export const NAV_DRAWER_START = asyncComputed<NavItem[]>(async() => {
+  const { username } = await useSession()
+  return [
+    {
+      items: [
+        {
+          to: '/',
+          label: 'Overview',
+          icon: 'i-carbon:home',
+        },
+        {
+          to: { name: 'Workspace', params: { workspace: username } },
+          label: 'Workspace',
+          icon: 'i-carbon:flow',
+        },
+        {
+          to: '/modules',
+          label: 'Modules',
+          icon: 'i-carbon:box',
+        },
+        {
+          to: '/integrations',
+          label: 'Integrations',
+          icon: 'i-carbon:integration',
+        },
+        {
+          to: '/storage',
+          label: 'Storage',
+          icon: 'i-carbon:db2-database',
+        },
+        {
+          to: '/keys',
+          label: 'API Keys',
+          icon: 'i-carbon:api-key',
+        },
+      ],
+    },
+    {
+      label: 'Organization',
+      items: [
+        {
+          to: '/members',
+          label: 'Members',
+          icon: 'i-carbon:user',
+        },
+        {
+          to: '/settings',
+          label: 'Settings',
+          icon: 'i-carbon:settings',
+        },
+        {
+          to: '/monitoring',
+          label: 'Monitoring',
+          icon: 'i-carbon:cloud-monitoring',
+        },
+        {
+          to: '/usage',
+          label: 'Usage',
+          icon: 'i-carbon:analytics',
+        },
+        {
+          to: '/billing',
+          label: 'Billing',
+          icon: 'i-carbon:document',
+        },
+      ],
+    },
+  ]
+})
 
 export const NAV_DRAWER_END = computed<NavItem[]>(() => [
   {
