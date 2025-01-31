1acd07252213cbd1cbeff900e83c337ecba34a95
Stanley Horwood <stanley.horwood@nanoworks.io>
2025-01-31 19:08:44 +0100

    chore(runner): enhance authorization tests for events and peers
    This commit significantly expands the test coverage for the `authorize` function in the runner package. The existing tests for event authorization have been refined, and a new set of tests for peer authorization has been added. These changes ensure that the `authorize` function correctly handles both H3Event and Peer objects, validating token and address checks under various scenarios.
    
    The tests now cover cases with different `runnerTrustProxy` settings, invalid tokens, and invalid addresses for both events and peers. This comprehensive test suite will help maintain the integrity of the authorization process as the codebase evolves, ensuring that both HTTP requests and WebSocket connections are properly secured.

--- packages/runner/utils/authorize.test.ts
+++ packages/runner/utils/authorize.test.ts
@@ -1,5 +1,5 @@
 import type { Context } from '../__fixtures__'
-import { createContext, createEvent } from '../__fixtures__'
+import { createContext, createEvent, createPeer } from '../__fixtures__'
 import { authorize } from './authorize'
 
 describe.concurrent<Context>('authorize', () => {
@@ -13,17 +13,88 @@ describe.concurrent<Context>('authorize', () => {
   })
 
   describe<Context>('with event', (it) => {
-    it('should authorize the event', ({ expect, ctx }) => {
+    it('should authorize the event when the token and address are valid with "runnerTrustProxy" set to true', ({ ctx }) => {
       ctx.ModuleRunner.runnerTrustProxy = true
       const event = createEvent({
-        remoteAddress: ctx.ModuleRunner.runnerMasterAddress,
         headers: {
-          'Authorization': `Bearer ${ctx.ModuleRunner.runnerToken}`,
-          'X-Forwarded-For': ctx.ModuleRunner.runnerMasterAddress,
+          'authorization': `Bearer ${ctx.ModuleRunner.runnerToken}`,
+          'x-forwarded-for': ctx.ModuleRunner.runnerMasterAddress,
         },
       })
       const result = authorize.call(ctx.ModuleRunner, event)
-      expect(result).toStrictEqual(undefined)
+      expect(result).toBeUndefined()
+    })
+
+    it('should authorize the event when the token and address are valid with "runnerTrustProxy" set to false', ({ ctx }) => {
+      const event = createEvent({
+        headers: { authorization: `Bearer ${ctx.ModuleRunner.runnerToken}` },
+        remoteAddress: ctx.ModuleRunner.runnerMasterAddress,
+      })
+      const result = authorize.call(ctx.ModuleRunner, event)
+      expect(result).toBeUndefined()
+    })
+
+    it('should throw an error when the token is invalid', ({ ctx }) => {
+      const event = createEvent({
+        headers: { authorization: 'Bearer invalid' },
+        remoteAddress: ctx.ModuleRunner.runnerMasterAddress,
+      })
+      const shouldThrow = () => authorize.call(ctx.ModuleRunner, event)
+      const error = ctx.ModuleRunner.errors.NOT_AUTHORIZED()
+      expect(shouldThrow).toThrowError(error)
+    })
+
+    it('should throw an error when the address is invalid', ({ ctx }) => {
+      const event = createEvent({
+        headers: { authorization: `Bearer ${ctx.ModuleRunner.runnerToken}` },
+        remoteAddress: '0.0.0.0',
+      })
+      const shouldThrow = () => authorize.call(ctx.ModuleRunner, event)
+      const error = ctx.ModuleRunner.errors.NOT_AUTHORIZED()
+      expect(shouldThrow).toThrowError(error)
+    })
+  })
+
+  describe<Context>('with peer', (it) => {
+    it('should authorize the peer when the token and address are valid with "runnerTrustProxy" set to true', ({ ctx }) => {
+      ctx.ModuleRunner.runnerTrustProxy = true
+      const peer = createPeer({
+        headers: {
+          'authorization': `Bearer ${ctx.ModuleRunner.runnerToken}`,
+          'x-forwarded-for': ctx.ModuleRunner.runnerMasterAddress,
+        },
+      })
+      const result = authorize.call(ctx.ModuleRunner, peer)
+      expect(result).toBeUndefined()
+    })
+
+    it('should authorize the peer when the token and address are valid with "runnerTrustProxy" set to false', ({ ctx }) => {
+      const peer = createPeer({
+        headers: { authorization: `Bearer ${ctx.ModuleRunner.runnerToken}` },
+        remoteAddress: ctx.ModuleRunner.runnerMasterAddress,
+      })
+      const result = authorize.call(ctx.ModuleRunner, peer)
+      expect(result).toBeUndefined()
+    })
+
+    it('should throw an error when the token is invalid', ({ ctx }) => {
+      const peer = createPeer({
+        headers: { authorization: 'Bearer invalid' },
+        remoteAddress: ctx.ModuleRunner.runnerMasterAddress,
+      })
+      const shouldThrow = () => authorize.call(ctx.ModuleRunner, peer)
+      const error = ctx.ModuleRunner.errors.NOT_AUTHORIZED()
+      expect(shouldThrow).toThrowError(error)
+    })
+
+    it('should throw an error when the address is invalid', ({ ctx }) => {
+      const peer = createPeer({
+        headers: { authorization: `Bearer ${ctx.ModuleRunner.runnerToken}` },
+        remoteAddress: '0.0.0.0',
+      })
+      const shouldThrow = () => authorize.call(ctx.ModuleRunner, peer)
+      const error = ctx.ModuleRunner.errors.NOT_AUTHORIZED()
+      expect(shouldThrow).toThrowError(error)
     })
   })
 })
--- packages/runner/utils/authorize.ts
+++ packages/runner/utils/authorize.ts
@@ -1,8 +1,9 @@
+import type { Peer } from 'crossws'
 import type { H3Event } from 'h3'
 import type { ModuleRunner } from '..'
 import { getEventInformation } from './getEventInformation'
 
-export function authorize(this: ModuleRunner, event: H3Event) {
+export function authorize(this: ModuleRunner, event: H3Event | Peer) {
   const { token, address } = getEventInformation.call(this, event)
   if (token !== this.runnerToken) throw this.errors.NOT_AUTHORIZED()
   if (address !== this.runnerMasterAddress) throw this.errors.NOT_AUTHORIZED()
