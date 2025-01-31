8cb830e0f8bc8d3222ec456ae8323fa432814a6d
Stanley Horwood <stanley.horwood@nanoworks.io>
2025-01-31 19:08:45 +0100

    chore(api): enhance session cookie configuration
    This commit improves the configuration of the session cookie in the `setSessionCookie` function within the user module of the API package. The function now sets additional properties for the cookie, including the `path` and `expires` attributes. The `path` is set to '/api' to restrict the cookie's scope, and the `expires` attribute is set to match the session's expiration time. These changes enhance security and provide more precise control over the cookie's lifecycle.
    
    The commit also updates the function's documentation to more accurately describe its purpose and parameters.

--- packages/api/user/utils/setSessionCookie.ts
+++ packages/api/user/utils/setSessionCookie.ts
@@ -5,7 +5,7 @@ import { setCookie } from 'h3'
 import { createCipheriv, createHash } from 'node:crypto'
 
 /**
- * Given a session token, set the session cookie in the response headers.
+ * Given an `H3Event` and a `UserSession`, set the session cookie in the response headers.
  *
  * @param event The H3 event object.
  * @param session The user session to set the cookie for.
@@ -22,6 +22,8 @@ export function setSessionCookie(this: ModuleUser, event: H3Event, session: User
     secure: true,
     httpOnly: true,
     sameSite: 'strict',
+    path: '/api',
+    expires: session.expiresAt,
     maxAge: (session.expiresAt.getTime() - Date.now()) / 1000,
   })
 }
