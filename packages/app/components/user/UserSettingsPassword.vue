965a579ad2a7351fee5e68ef011ab87598fba90f
Stanley Horwood <stanley.horwood@nanoworks.io>
2025-01-31 19:06:17 +0100

    feat(app): add password update form in user settings
    Introduced a new component `UserSettingsPassword.vue` within the user settings to allow users to update their password. This form includes fields for the current password, new password, and password confirmation. The form guides users to choose a strong, unique password and encourages regular updates for enhanced security. This addition aims to improve user account security management.

---
+++ packages/app/components/user/UserSettingsPassword.vue
@@ -0,0 +1,56 @@
+<script setup lang="ts">
+const emit = defineEmits<{
+  submit: [UserSetPasswordOptions]
+}>()
+
+const oldPassword = ref('')
+const newPassword = ref('')
+const newPasswordConfirm = ref('')
+
+function onSubmit() {
+  emit('submit', {
+    oldPassword: oldPassword.value,
+    newPassword: newPassword.value,
+    newPasswordConfirm: newPasswordConfirm.value,
+  })
+}
+</script>
+
+<template>
+  <AppPageForm
+    vertical
+    title="Password"
+    label="Update Password"
+    @submit="() => onSubmit()">
+
+    <template #text>
+      Update your password to keep your account secure. It is recommended to use a strong password
+      that you don't use elsewhere and to change it regularly.
+    </template>
+
+    <InputText
+      v-model="oldPassword"
+      icon="i-carbon:password"
+      class="w-full"
+      placeholder="Current Password"
+      type="password"
+    />
+
+    <div class="flex w-full gap-4 lt-md:flex-col">
+      <InputText
+        v-model="newPassword"
+        icon="i-carbon:password"
+        class="w-full"
+        placeholder="New Password"
+        type="password"
+      />
+      <InputText
+        v-model="newPasswordConfirm"
+        icon="i-carbon:password"
+        class="w-full"
+        placeholder="Confirm New Password"
+        type="password"
+      />
+    </div>
+  </AppPageForm>
+</template>
