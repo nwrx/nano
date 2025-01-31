cf53dabfe2f4c5c9d4c598d69436adcaf81ee08b
Stanley Horwood <stanley.horwood@nanoworks.io>
2025-01-31 19:06:23 +0100

    feat(app): update styling and localization handling in `AppDialog`
    Refactored the `AppDialog` component for improved styling and localization support. Enhanced the dialog's visual design by adjusting its backdrop brightness and introducing shadow and border changes to better support both light and dark themes. Additionally, the localization keys for button labels have been updated to ensure consistency across different languages, improving the UX for international users. This change contributes to a more cohesive and user-friendly interface in multi-language environments.

--- packages/app/components/app/AppDialog.vue
+++ packages/app/components/app/AppDialog.vue
@@ -1,21 +1,3 @@
-<i18n lang="yaml">
-en:
-  cancel: Cancel
-  confirm: Confirm
-fr:
-  cancel: Annuler
-  confirm: Confirmer
-de:
-  cancel: Abbrechen
-  confirm: Bestätigen
-es:
-  cancel: Cancelar
-  confirm: Confirmar
-zh:
-  cancel: 取消
-  confirm: 确认
-</i18n>
-
 <script setup lang="ts">
 import type { BaseDialogProps } from '@unshared/vue'
 
@@ -38,23 +20,31 @@ const { t, locale } = useI18n({ useScope: 'local' })
       v-bind="props"
       title=""
       :class="{
-        'backdrop-brightness-40': modelValue,
+        'backdrop-brightness-50': modelValue,
         'pointer-events-none': !modelValue,
       }"
       class="
         fixed left-0 top-0 z-50 w-full h-full scale-105 bg-transparent
         inline-flex items-center justify-center backdrop:bg-transparent
-        transition-all duration-200 p-0 dark
+        transition-all duration-100 dark
       ">
 
       <!-- Content -->
       <template #dialog="slots">
         <div
           class="
-            rounded-lg max-w-xl pointer-events-auto
-            bg-white dark:bg-primary-900
-            text-black dark:text-white
-            transition-all duration-100
+            rounded max-w-xl pointer-events-auto border
+             transition-all duration-100 backdrop-blur-lg
+
+            shadow-xl
+            light:bg-white/10
+            light:text-black
+            light:border-light
+
+            dark:shadow-2xl
+            dark:border-primary-900
+            dark:bg-primary-900
+            dark:text-white
           "
           :class="{
             'scale-100 opacity-100 pointer-events-auto': slots.isOpen,
@@ -87,8 +77,8 @@ const { t, locale } = useI18n({ useScope: 'local' })
           <div class="p-4 w-full bg-primary-500/10">
             <slot name="actions" v-bind="slots">
               <AppDialogActions
-                :labelCancel="labelCancel ?? t('cancel')"
-                :labelConfirm="labelConfirm ?? t('confirm')"
+                :labelCancel="labelCancel ?? t('button.cancel')"
+                :labelConfirm="labelConfirm ?? t('button.confirm')"
                 :variant="variant"
                 :disabled="disabled"
                 @close="slots.close"
@@ -101,3 +91,21 @@ const { t, locale } = useI18n({ useScope: 'local' })
     </LazyBaseDialog>
   </Teleport>
 </template>
+
+<i18n lang="yaml">
+  en:
+    button.cancel: Cancel
+    button.confirm: Confirm
+  fr:
+    button.cancel: Annuler
+    button.confirm: Confirmer
+  de:
+    button.cancel: Abbrechen
+    button.confirm: Bestätigen
+  es:
+    button.cancel: Cancelar
+    button.confirm: Confirmar
+  zh:
+    button.cancel: 取消
+    button.confirm: 确认
+  </i18n>
