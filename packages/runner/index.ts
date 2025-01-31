369b161e7077a894d5e6b89c6a5c9021bddd25a1
Stanley Horwood <stanley.horwood@nanoworks.io>
2025-01-31 19:08:43 +0100

    feat(runner): enhance ModuleRunner with additional configuration options
    This commit introduces several improvements to the ModuleRunner class. It adds new properties for better configuration and security, including a runnerToken, runnerIsClaimed flag, runnerTrustProxy flag, and runnerMasterAddress. The worker pool size calculation has been adjusted to use one less than the number of CPU cores, potentially improving resource allocation.
    
    The commit also reorganizes imports, adds an errors property referencing ERRORS from utils, and introduces an empty entities object for future use. These changes provide more flexibility and control over the runner's behavior, enhancing its capabilities for various deployment scenarios.

--- packages/runner/index.ts
+++ packages/runner/index.ts
@@ -1,17 +1,24 @@
 import type { WorkerPool } from '@unshared/process'
-import type { ThreadWorkerSession } from './utils'
+import type { ThreadWorkerSession } from './worker'
 import { ModuleBase } from '@unserved/server'
 import { createWorkerPool } from '@unshared/process'
 import { randomUUID } from 'node:crypto'
 import { cpus } from 'node:os'
 import * as ROUTES from './routes'
+import { ERRORS } from './utils'
 
 export class ModuleRunner extends ModuleBase {
   routes = ROUTES
+  errors = ERRORS
+  entities = {}
 
-  runnerSecret = randomUUID()
+  runnerToken = randomUUID()
+  runnerIsClaimed = false
+  runnerTrustProxy = false
+  runnerMasterAddress = '127.0.0.1'
+
   runnerWorkerPool: WorkerPool
-  runnerWorkerPoolSize = cpus().length * 2 - 1
+  runnerWorkerPoolSize = cpus().length - 1
   runnerSessions = new Map<string, ThreadWorkerSession>()
 
   constructor() {
@@ -20,7 +27,6 @@ export class ModuleRunner extends ModuleBase {
       resourceLimits: { stackSizeMb: 8 },
       argv: ['--untrusted-code-mitigations'],
       size: this.runnerWorkerPoolSize,
-      eager: true,
     })
   }
 }
