50b0aea0d089f782b3bb0105803ba3411b41db94
Stanley Horwood <stanley.horwood@nanoworks.io>
2025-01-31 19:07:03 +0100

    chore(module-core): standardize validation and parsing functions
    Standardized the use of validation utilities across `file`, `stream`, and `string` types in the `module-core` package. Replaced usage of `createAssertInstance` with `assert.instance` for consistency and clarity. Additionally, enhanced the `string` type parser with more descriptive transformations for boolean and number types, improving readability and maintainability of the parsing logic.

--- packages/module-core/types/stream.ts
+++ packages/module-core/types/stream.ts
@@ -1,13 +1,10 @@
-/* eslint-disable n/no-unsupported-features/node-builtins */
 import { defineType } from '@nwrx/core'
-import { createAssertInstance, createParser } from '@unshared/validation'
+import { assert, createParser } from '@unshared/validation'
 
 export const stream = defineType({
   kind: 'stream',
   name: 'Stream',
   color: '#EEDCFF',
   description: 'A stream of data that can be read or written.',
-  parse: createParser(
-    [createAssertInstance(ReadableStream<Uint8Array>)],
-  ),
+  parse: createParser(assert.instance(ReadableStream<Uint8Array>)),
 })
