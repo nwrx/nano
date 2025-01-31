14574456fc1da4f8ab4bae76e5c0ba835edd376d
Stanley Horwood <stanley.horwood@nanoworks.io>
2025-01-31 19:05:55 +0100

    feat(api): update endpoint and variable naming in `userSignout` route
    The user signout route's endpoint has been updated from `/api/signout` to `/api/session` to align with RESTful standards, enhancing clarity and consistency in our API. Additionally, the internal variable `userSession` has been renamed to `session` to simplify the code and improve readability, making it easier for developers to understand and maintain the codebase. This change contributes to a cleaner and more intuitive server module.

--- packages/api/user/routes/userSignout.ts
+++ packages/api/user/routes/userSignout.ts
@@ -3,10 +3,10 @@ import { createRoute } from '@unserved/server'
 import { deleteCookie, setResponseStatus } from 'h3'
 
 export function userSignout(this: ModuleUser) {
-  return createRoute('DELETE /api/signout', async({ event }) => {
+  return createRoute('DELETE /api/session', async({ event }) => {
     const { UserSession } = this.getRepositories()
-    const { userSession } = await this.authenticate(event)
-    await UserSession.softRemove(userSession)
+    const session = await this.authenticate(event)
+    await UserSession.softRemove(session)
     setResponseStatus(event, 204)
     deleteCookie(event, this.userSessionCookieName)
   })
