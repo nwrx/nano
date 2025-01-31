b1b2c97b2b61d03b4ceffcfae4152dd30c409f67
Stanley Horwood <stanley.horwood@nanoworks.io>
2025-01-31 19:07:23 +0100

    chore(app): adjust padding in tooltip description
    Modified the padding of the description in the tooltip for `EditorNode.Header.vue` to improve visual alignment and readability. The top padding was increased to match the existing padding styles, ensuring a consistent look and feel across tooltip elements.

--- packages/app/components/editor/EditorNode.Header.vue
+++ packages/app/components/editor/EditorNode.Header.vue
@@ -52,7 +52,7 @@ function handleGrab(event: MouseEvent) {
       <!-- Tooltip content -->
       <template #tooltip>
         <div class="w-99 divide-y divide-editor">
-          <p v-if="description" v-markdown="description" class="px-md py-0 text-app markdown" />
+          <p v-if="description" v-markdown="description" class="px-md pt-md text-app markdown" />
           <p class="px-md py-sm font-medium text-app">
             (node): {{ kind }}
           </p>
