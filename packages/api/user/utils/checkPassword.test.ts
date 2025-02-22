df3e06828b08181fd48a290b06028e27b7fb2324
Stanley Horwood <stanley.horwood@nanoworks.io>
2025-02-22 11:13:58 +0100

    chore(api): refactor password check tests for better maintainability
    The password checking test suite has been refactored to use explicit user and password creation instead of relying on helper functions. This change improves test clarity by making the setup process more transparent and reduces hidden dependencies. The tests now use a constant for the test password and properly handle user and password entity persistence separately.

--- packages/api/user/utils/checkPassword.test.ts
+++ packages/api/user/utils/checkPassword.test.ts
@@ -1,7 +1,11 @@
+/* eslint-disable sonarjs/no-hardcoded-passwords */
 import type { Context } from '../../__fixtures__'
-import { createTestContext } from '../../__fixtures__'
+import { createTestContext, FIXTURE_USER_BASIC } from '../../__fixtures__'
 import { checkPassword } from './checkPassword'
 import { createPassword } from './createPassword'
+import { createUser } from './createUser'
+
+const PASSWORD = 'password'
 
 describe.concurrent('checkPassword', () => {
   beforeEach<Context>(async(context) => {
@@ -14,42 +18,51 @@ describe.concurrent('checkPassword', () => {
   })
 
   describe<Context>('matching', (it) => {
-    it('should return true for a matching password', async({ moduleUser, createUser }) => {
-      const { user, password } = await createUser()
-      const result = await checkPassword.call(moduleUser, user, password)
+    it('should return true for a matching password', async({ moduleUser }) => {
+      const user = await createUser.call(moduleUser, FIXTURE_USER_BASIC)
+      const password = await createPassword.call(moduleUser, { user, password: PASSWORD })
+      await moduleUser.getRepositories().User.save(user)
+      await moduleUser.getRepositories().UserPassword.save(password)
+      const result = await checkPassword.call(moduleUser, user, PASSWORD)
       expect(result).toBe(true)
     })
   })
 
   describe<Context>('mismatching', (it) => {
-    it('should return false for a non-matching password', async({ moduleUser, createUser }) => {
-      const { user } = await createUser()
-      const result = await checkPassword.call(moduleUser, user, 'wrong-password')
+    it('should return false for a non-matching password', async({ moduleUser }) => {
+      const user = await createUser.call(moduleUser, FIXTURE_USER_BASIC)
+      const password = await createPassword.call(moduleUser, { user, password: PASSWORD })
+      await moduleUser.getRepositories().User.save(user)
+      await moduleUser.getRepositories().UserPassword.save(password)
+      const result = await checkPassword.call(moduleUser, user, 'not-the-password')
       expect(result).toBe(false)
     })
 
-    it('should return false even if matching an old password', async({ moduleUser, createUser }) => {
-      const { user, password: oldPassword } = await createUser()
-      const newPassword = await createPassword.call(moduleUser, 'new-password')
-      user.passwords!.push(newPassword)
+    it('should return false even if matching an old password', async({ moduleUser }) => {
+      const user = await createUser.call(moduleUser, FIXTURE_USER_BASIC)
+      const passwordOld = await createPassword.call(moduleUser, { user, password: PASSWORD })
+      const passwordNew = await createPassword.call(moduleUser, { user, password: 'new-password' })
       await moduleUser.getRepositories().User.save(user)
-      const result = await checkPassword.call(moduleUser, user, oldPassword)
+      await moduleUser.getRepositories().UserPassword.save(passwordOld)
+      await moduleUser.getRepositories().UserPassword.save(passwordNew)
+      const result = await checkPassword.call(moduleUser, user, PASSWORD)
       expect(result).toBe(false)
     })
 
-    it('should return false if the password has expired', async({ moduleUser, createUser }) => {
-      const { user, password } = await createUser()
-      user.passwords![0].expiredAt = new Date(Date.now() - 1000)
+    it('should return false if the password has expired', async({ moduleUser }) => {
+      const user = await createUser.call(moduleUser, FIXTURE_USER_BASIC)
+      const password = await createPassword.call(moduleUser, { user, password: PASSWORD })
+      password.expiredAt = new Date(Date.now() - 1000)
       await moduleUser.getRepositories().User.save(user)
-      const result = await checkPassword.call(moduleUser, user, password)
+      await moduleUser.getRepositories().UserPassword.save(password)
+      const result = await checkPassword.call(moduleUser, user, PASSWORD)
       expect(result).toBe(false)
     })
 
-    it('should return false if no password is stored for the user', async({ moduleUser, createUser }) => {
-      const { user, password } = await createUser()
-      user.passwords = []
+    it('should return false if no password is stored for the user', async({ moduleUser }) => {
+      const user = await createUser.call(moduleUser, FIXTURE_USER_BASIC)
       await moduleUser.getRepositories().User.save(user)
-      const result = await checkPassword.call(moduleUser, user, password)
+      const result = await checkPassword.call(moduleUser, user, PASSWORD)
       expect(result).toBe(false)
     })
   })
