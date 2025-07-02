6305f0d8ac7bb7ce529232aa1c9f30677cae3aee
Stanley Horwood <stanley.horwood@nanoworks.io>
2025-07-02 18:41:10 +0200

    feat(app): add page form tabs and tab item components
    Introduced two new Vue components for creating form tabs: `AppPageForm.Tabs.vue` and `AppPageForm.TabsItem.vue`. These components provide a flexible and reusable way to create tabbed interfaces within forms, with built-in toggle functionality and styling. The implementation supports generic typing and uses the `BaseInputToggle` component to manage tab state and interactions.
    
    The `AppPageForm.Tabs.vue` serves as a container for tab items, providing a consistent layout and styling. The `AppPageForm.TabsItem.vue` component handles individual tab rendering, with dynamic active state styling and support for custom labels or content. These components enhance the UI/UX of form-based interfaces by enabling easy tab navigation and visual feedback.

---
+++ packages/app/components/app/AppPageForm.Tabs.vue
@@ -0,0 +1,5 @@
+<template>
+  <div class="w-full flex items-center gap-8 b-b b-app">
+    <slot />
+  </div>
+</template>
---
+++ packages/app/components/app/AppPageForm.TabsItem.vue
@@ -0,0 +1,33 @@
+<!-- eslint-disable vue/prop-name-casing -->
+<script setup lang="ts" generic="T, U extends ToggleType">
+import type { BaseInputToggleProps } from '@unshared/vue/BaseInputToggle'
+import type { ToggleType, ToggleValue } from '@unshared/vue/useBaseInputToggle'
+import { BaseInputToggle } from '@unshared/vue/BaseInputToggle'
+
+const props = defineProps<BaseInputToggleProps<T, U> & {
+  label?: string
+}>()
+
+const model = defineModel<ToggleValue<T, U>>()
+</script>
+
+<template>
+  <BaseInputToggle
+    v-slot="{ isActive }"
+    v-bind="props"
+    v-model="model"
+    eager
+    class="cursor-pointer select-none"
+    as="div">
+    <div
+      class="pb-4 b-b-2 -mb-px"
+      :class="{
+        'text-app b-active': isActive,
+        'text-subtle b-transparent hover:text-app hover:b-app': !isActive,
+      }">
+      <slot>
+        {{ label }}
+      </slot>
+    </div>
+  </BaseInputToggle>
+</template>
