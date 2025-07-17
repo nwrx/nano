aae00eb3875abd20efd829d193c5ccbb9c36af44
Stanley Horwood <stanley.horwood@nanoworks.io>
2025-07-17 18:33:08 +0200

    chore(app): add new `VerticalTabs.Button` component
    The `VerticalTabs.Button` component was created to provide a standardized button implementation for vertical tab navigation. It extends the `BaseButton` with additional props for icons and labels, and includes styling for active and hover states. The component uses flexbox for centering and includes conditional classes for visual feedback.

---
+++ packages/app/components/base/VerticalTabs.Button.vue
@@ -0,0 +1,30 @@
+<!-- eslint-disable vue/prop-name-casing -->
+<script setup lang="ts">
+import type { BaseButtonProps } from '@unshared/vue'
+
+const props = defineProps<BaseButtonProps & {
+  icon?: string
+  label?: string
+}>()
+</script>
+
+<template>
+  <BaseButton
+    v-bind="props"
+    eager>
+    <div
+      class="
+        flex flex-col items-center justify-center size-16 cursor-pointer
+        b b-transparent hover:b-active
+      "
+      :class="{
+        'bg-active b-prominent': isActive === true,
+      }">
+      <BaseIcon
+        :icon="icon"
+        class="size-6"
+        :class="{ 'text-active': isActive }"
+      />
+    </div>
+  </BaseButton>
+</template>
