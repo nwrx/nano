7a58c73f9d3490bba5108079b921030a70d452e4
Stanley Horwood <stanley.horwood@nanoworks.io>
2025-08-01 03:19:16 +0200

    chore(app): adjust background opacity for editor
    The opacity of the editor background has been modified from `op-10` to `op-20` when in light mode. This subtle change provides slightly more visibility of the background pattern, enhancing the visual depth of the editor interface.

--- packages/app/components/editor/EditorBackground.vue
+++ packages/app/components/editor/EditorBackground.vue
@@ -4,7 +4,7 @@ import PATTERN_EDITOR_URL from '~/assets/pattern-editor.svg?no-inline'
 
 <template>
   <div
-    class="absolute top-0 left-0 right-0 bottom-0 dark:op-10 op-10 dark:invert"
+    class="absolute top-0 left-0 right-0 bottom-0 dark:op-10 op-20 dark:invert"
     :style="{
       backgroundImage: `url('${PATTERN_EDITOR_URL}')`,
       backgroundSize: '60px',
