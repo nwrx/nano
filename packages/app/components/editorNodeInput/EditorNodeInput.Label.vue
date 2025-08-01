9afeaa40dac83384f77b6f909310f2a649204070
Stanley Horwood <stanley.horwood@nanoworks.io>
2025-08-01 06:34:38 +0200

    chore(editorNodeInput): improve label component layout and typing
    The EditorNodeInput.Label component has been updated to enhance its visual presentation and type safety. The changes include adding a flex layout, introducing an optional dash separator with a BaseIcon, and making the `name` and `schema` props required. These modifications improve the component's flexibility and readability while ensuring more strict type checking.

--- packages/app/components/editorNodeInput/EditorNodeInput.Label.vue
+++ packages/app/components/editorNodeInput/EditorNodeInput.Label.vue
@@ -2,13 +2,25 @@
 import type { Schema } from '@nwrx/nano/utils'
 
 defineProps<{
-  name?: string
-  schema?: Schema
+  name: string
+  schema: Schema
+  hideDash?: boolean
 }>()
 </script>
 
 <template>
-  <p class="text-sm truncate text-subtle shrink-0 mr-sm font-mono">
-    {{ schema?.title || name || 'MISSING_SCHEMA_AND_NAME' }} ‒
+  <p class="flex items-center text-sm truncate text-subtle shrink-0 mr-sm font-mono gap-sm">
+
+    <!-- Label -->
+    <span>
+      {{ schema.title || name || 'MISSING_SCHEMA_AND_NAME' }}
+    </span>
+
+    <!-- Separator -->
+    <BaseIcon
+      v-if="!hideDash"
+      icon="i-carbon:arrow-right"
+      class="w-3 h-3 shrink-0"
+    />
   </p>
 </template>
