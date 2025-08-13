e1d149338bfbb661f80fcfc3f6b3911a5c50d2f1
Stanley Horwood <stanley.horwood@nanoworks.io>
2025-08-13 08:47:07 +0200

    feat(app): add reusable `Hint` component for displaying contextual information
    Introduced a new `Hint` component that provides a flexible way to display contextual information with optional icons and markdown support. The component allows rendering hints through either a text prop or a default slot, supporting dynamic content and visual customization. It includes built-in markdown rendering and icon display, making it easy to create informative and visually consistent hints across the application.

---
+++ packages/app/components/base/Hint.vue
@@ -0,0 +1,27 @@
+<script setup lang="ts">
+import { BaseIcon } from '@unshared/vue/BaseIcon'
+import { vMarkdown } from '~/utils/vMarkdown'
+
+defineProps<{
+  text?: string
+  icon?: string
+  classHint?: string
+}>()
+</script>
+
+<template>
+  <div class="hint w-full flex space-x-md p-md rd">
+
+    <!-- Icon -->
+    <BaseIcon
+      v-if="icon"
+      :icon="icon"
+      class="size-4 shrink-0 mt-xs"
+    />
+
+    <!-- Text -->
+    <slot>
+      <p v-markdown="text" class="text-sm" />
+    </slot>
+  </div>
+</template>
