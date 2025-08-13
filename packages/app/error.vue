65af7684554ee394d4d17020ab5d203ae7e41656
Stanley Horwood <stanley.horwood@nanoworks.io>
2025-08-13 08:55:36 +0200

    chore(app): simplify error page stack trace handling
    Removed the complex stack trace transformation logic in the error page. The stack trace is now directly passed from the error object when in development mode, reducing unnecessary string manipulation and improving readability of the error handling code.

--- packages/app/error.vue
+++ packages/app/error.vue
@@ -9,15 +9,6 @@ const props = defineProps<{
 
 const router = useRouter()
 const showStack = import.meta.dev
-
-const stack = computed(() => {
-  if (!props.error) return ''
-  return props.error.stack
-    ?.replaceAll('\n', '\n  ')
-    .replaceAll(/https?:\/\/[^:]+:\d+\/static\/@fs\//g, '/')
-    .replaceAll('/home/shorwood/Workspaces/', '')
-    .replaceAll(/\.pnpm\/[^/]+\/node_modules\//g, '')
-})
 </script>
 
 <template>
@@ -25,7 +16,7 @@ const stack = computed(() => {
     <AppPageErrorBoundary
       :status-code="props.error.statusCode"
       :message="props.error.message"
-      :stack="showStack ? stack : undefined"
+      :stack="showStack ? props.error.stack : undefined"
       @clear-error="() => router.push('/')"
     />
   </NuxtLayout>
