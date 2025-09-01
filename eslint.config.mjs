c23d5382dc590bd33aac58f05db9659532988dd5
Stanley Horwood <stanley.horwood@nanoworks.io>
2025-09-01 20:01:55 +0200

    chore(eslint): integrate turbo eslint configuration
    The project's ESLint configuration has been updated to include Turbo's recommended flat config. Additional rule overrides have been added to disable specific lint rules that were causing issues or are not applicable to the current project setup. This change aims to improve code quality and consistency across the project while providing flexibility for unique project requirements.

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,9 +1,12 @@
 import unshared from '@unshared/eslint-config'
+import turboConfig from 'eslint-config-turbo/flat'
 
 export default [
+  ...turboConfig,
   ...unshared(),
   {
     rules: {
+      'n/hashbang': 'off',
       'vue/valid-v-slot': 'off',
       'sonarjs/new-cap': 'off',
       'sonarjs/void-use': 'off',
